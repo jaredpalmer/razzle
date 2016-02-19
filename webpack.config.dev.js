b658cb7e00c0f687cc9e7976ff7b23343553f97d
Jared Palmer <palmdeezy>
2016-02-18 19:15:05 -0500

    add back react hot loader

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -27,7 +27,7 @@ module.exports = {
   module: {
     loaders: [{
       test: /\.js$/,
-      loader: 'babel-loader?presets[]=es2015&presets[]=react&presets[]=stage-0',
+      loaders: ['react-hot', 'babel'],
       include: path.join(__dirname, 'src')
     }]
   }
