691a5b4e966e59614daf8d16c135507854b73d6f
Jared Palmer <palmdeezy>
2016-02-22 08:38:22 -0500

    Add react-helmet title to edit page

--- src/routes/Edit/components/Edit.js
+++ src/routes/Edit/components/Edit.js
@@ -1,6 +1,12 @@
 import React, { PropTypes } from 'react';
+import Helmet from 'react-helmet';
 
 const Edit = (props) =>
-  <div>Edit Page</div>;
+  <div>
+    <Helmet
+      title='Edit'
+    />
+    Edit Page
+  </div>;
 
 export default Edit;
