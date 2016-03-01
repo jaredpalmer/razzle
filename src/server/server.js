e16e9829735e681a231dc473dfc1fb59b88d8a7c
Jared Palmer <palmdeezy>
2016-03-01 10:36:18 +0900

    update asset.json logic for dev to work

--- src/server/server.js
+++ src/server/server.js
@@ -22,7 +22,6 @@ import { callAPIMiddleware } from '../middleware/callAPIMiddleware';
 import { StyleSheetServer } from 'aphrodite';
 import { configureStore } from '../store';
 import Helm from 'react-helmet'; // because we are already using helmet
-import assets from '../../assets.json';
 import reducer from '../createReducer';
 import createRoutes from '../routes/root';
 
@@ -58,6 +57,9 @@ server.use(compression());
 server.use('/api/v0/posts', require('./api/posts'));
 server.use('/api/v0/post', require('./api/post'));
 
+// Stub for assets, in case running in dev mode.
+let assets;
+
 // Webpack (for development)
 if (isDeveloping) {
   server.use(morgan('dev'));
@@ -81,6 +83,7 @@ if (isDeveloping) {
     log: console.log,
   }));
 } else {
+  assets = require('../../assets.json');
   server.use(morgan('combined'));
   server.use('/build/static', express.static(__dirname + '../../../build/static'));
 }
@@ -100,6 +103,198 @@ const renderFullPage = (data, initialState, assets) => {
          <meta name="viewport" content="width=device-width, initial-scale=1" />
          ${head.meta.toString()}
          ${head.link.toString()}
+         <style>
+
+           html {
+             box-sizing: border-box;
+           }
+
+           *,
+           *::before,
+           *::after {
+             box-sizing: border-box;
+           }
+
+           @at-root {
+             @-moz-viewport      { width: device-width; }
+             @-ms-viewport       { width: device-width; }
+             @-o-viewport        { width: device-width; }
+             @-webkit-viewport   { width: device-width; }
+             @viewport           { width: device-width; }
+           }
+
+           html {
+           	font-size: 100%;
+           	-ms-overflow-style: scrollbar;
+           	-webkit-tap-highlight-color: rgba(0,0,0,0);
+           	height: 100%;
+           }
+
+           body {
+           	font-size: 1rem;
+            background-color: #fff;
+           	color: #555;
+           	-webkit-font-smoothing: antialiased;
+             -moz-osx-font-smoothing: grayscale;
+           	font-family: -apple-system,BlinkMacSystemFont,"Helvetica Neue",Helvetica,Arial,sans-serif;
+           }
+
+           [tabindex="-1"]:focus {
+             outline: none !important;
+           }
+
+           /* Typography */
+
+           h1, h2, h3, h4, h5, h6 {
+             margin: 0;
+             letter-spacing: -0.03em;
+             font-weight: bold;
+           }
+
+           p {
+             margin: 0;
+           }
+
+           abbr[title],
+           abbr[data-original-title] {
+             cursor: help;
+             border-bottom: 1px dotted #eee;
+           }
+
+           address {
+             margin-bottom: 1rem;
+             font-style: normal;
+             line-height: inherit;
+           }
+
+           ol,
+           ul,
+           dl {
+             margin-top: 0;
+             margin-bottom: 1rem;
+           }
+
+           ol ol,
+           ul ul,
+           ol ul,
+           ul ol {
+             margin-bottom: 0;
+           }
+
+           dt {
+             font-weight: bold;
+           }
+
+           dd {
+             margin-bottom: .5rem;
+             margin-left: 0;
+           }
+
+           blockquote {
+             margin: 0 0 1rem;
+           }
+
+           /* Links */
+           a,
+           a:hover,
+           a:focus {
+           	text-decoration: none;
+           }
+
+
+           /* Code */
+           pre {
+             margin: 0;
+             /*margin-bottom: 1rem;*/
+           }
+           /* Figures & Images */
+           figure {
+             margin: 0 0 1rem;
+           }
+
+           img {
+           	vertical-align: middle;
+           }
+
+           [role="button"] {
+             cursor: pointer;
+           }
+
+           a,
+           area,
+           button,
+           [role="button"],
+           input,
+           label,
+           select,
+           summary,
+           textarea {
+             touch-action: manipulation;
+           }
+
+           /* Forms */
+
+           label {
+           	display: inline-block;
+           	margin-bottom: .5rem;
+           }
+
+           button:focus {
+             outline: 1px dotted;
+             outline: 5px auto -webkit-focus-ring-color;
+           }
+
+           input,
+           button,
+           select,
+           textarea {
+             margin: 0;
+             line-height: inherit;
+             border-radius: 0;
+           }
+
+           textarea {
+             resize: vertical;
+           }
+
+           fieldset {
+             min-width: 0;
+             padding: 0;
+             margin: 0;
+             border: 0;
+           }
+
+           legend {
+             display: block;
+             width: 100%;
+             padding: 0;
+             margin-bottom: .5rem;
+             font-size: 1.5rem;
+             line-height: inherit;
+           }
+
+           input[type="search"],
+           input[type="text"],
+           textarea {
+             box-sizing: inherit;
+             -webkit-appearance: none;
+           }
+
+           [hidden] {
+             display: none !important;
+           }
+
+           body,
+           button,
+           input,
+           textarea {
+             font-family: font-family: 'Helvetica Neue', Helvetica, sans-serif;;
+           }
+
+           a,a:visited {
+             text-decoration: none;
+           }
+         </style>
          <style data-aphrodite>${data.css.content}</style>
       </head>
       <body>
