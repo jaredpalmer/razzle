2d9f0515f81c85ff9879c30a16a683cb8afddde6
Rowell Heria <owieh@me.com>
2016-03-11 15:59:57 +0000

    NotFound component added

---
+++ src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React, { PropTypes } from 'react';
+import Helmet from 'react-helmet';
+import { StyleSheet, css } from 'aphrodite';
+import { Type } from '../style';
+
+const NotFound = () =>
+  <div>
+    <Helmet title="Not Found" />
+  </div>;
+
+export default NotFound;
