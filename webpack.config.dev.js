f89e088f991749f39040aa850a0ff4a7352eb8e9
Jared Palmer <palmdeezy>
2016-03-18 11:10:14 +0800

    remove PROD webpack flag. redundant

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -34,7 +34,6 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('development'),
       '__DEV__': true,
-      '__PROD__': false,
     }),
   ],
   module: {
