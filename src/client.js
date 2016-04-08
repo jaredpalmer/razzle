7644839dcf9a59f2a8f8203a46aeba2de42e0113
Rowell Heria <owieh@me.com>
2016-04-08 22:10:55 +0100

    removed redbox-react

--- src/client.js
+++ src/client.js
@@ -74,25 +74,6 @@ let render = () => {
 };
 
 if (module.hot) {
-  // Support hot reloading of components
-  // and display an overlay for runtime errors
-  const renderApp = render;
-  const renderError = (error) => {
-    const RedBox = require('redbox-react');
-    ReactDOM.render(
-      <RedBox error={error} />,
-      container
-    );
-  };
-
-  render = () => {
-    try {
-      renderApp();
-    } catch (error) {
-      renderError(error);
-    }
-  };
-
   module.hot.accept('./routes/root', () => {
     setTimeout(render);
   });
