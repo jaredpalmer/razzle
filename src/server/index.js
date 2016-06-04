351d83a596d70218a2ecda4615393fc9ba784d69
Justin Greenberg <me@justingreenberg.com>
2016-06-04 02:32:06 -0400

    add node-hooks for ssr images/fonts
    simple custom hooks

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,3 @@
 require('babel-register');
-if (process.env.NODE_ENV != 'production') {
-  require.extensions['.png'] = function () {};
-  require.extensions['.jpg'] = function () {};
-  require.extensions['.jpeg'] = function () {};
-  require.extensions['.woff'] = function () {};
-  require.extensions['.woff2'] = function () {};
-  require.extensions['.ico'] = function () {};
-  require.extensions['.svg'] = function () {};
-}
+require('./node-hooks');
 require('./server');
