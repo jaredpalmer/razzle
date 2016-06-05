b53185c27b782ab4c9990d15b637b398843505c9
Justin Greenberg <me@justingreenberg.com>
2016-06-05 16:07:07 -0400

    Cache babel transforms in development

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -45,7 +45,10 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel',
-        query: { presets: ['es2015', 'react', 'stage-0'] },
+        query: {
+          cacheDirectory: true,
+          presets: ['es2015', 'react', 'stage-0']
+        },
         include: getPath('src')
       },
       {
