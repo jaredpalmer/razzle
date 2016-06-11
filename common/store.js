dcc9fbfdf052b5c993a985dad7b48db8f6527527
Justin Greenberg <me@justingreenberg.com>
2016-06-11 02:39:27 -0400

    axios-thunk middleware integration

--- common/store.js
+++ common/store.js
@@ -1,14 +1,15 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
+import axios from 'axios'
 import createReducer from './createReducer'
 
 export function configureStore (initialState) {
   let store = createStore(createReducer(), initialState, compose(
     applyMiddleware(
-      thunk
+      thunk.withExtraArgument({ axios })
     ),
 
-    (process.env.NODE_ENV === 'development') &&
+    process.env.NODE_ENV === 'development' &&
     typeof window === 'object' &&
     typeof window.devToolsExtension !== 'undefined'
       ? window.devToolsExtension()
