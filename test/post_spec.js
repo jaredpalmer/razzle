02c29a6729ce62465c32358c936ae56ea04d1938
Rowell <rowellx68@users.noreply.github.com>
2016-06-27 16:24:24 +0100

    ImmutableJS in Post and PostList reducers (#78)
    * refactor client, common, server
    
    * bump node version to 6.2.1
    
    * missing comma in package.json
    
    * update travis
    
    * not using RHL anymore
    
    * fix text decoration on the about page
    
    * clean up webpack, swap `indexRoute`
    
    This restore HMR. It also swaps the `indexRoute` of the application.
    The About page is now the primary view. PostList is now at `/posts`.
    
    * Rename About -> Home, remove PostList reducer
    
    * clean up styles
    
    * drop API middleware,
    
    Middleware is pretty opinionated, in order to make things work I would
    have needed to use a custom solution. This new code is agnostic with
    respect to whether you use `isomorphic-fetch` or `axios` or whatevs.
    
    * delete middleware docs
    
    * add demo selectors for reducers (#72)
    
    * add demo selectors for reducers
    
    * fix tests
    
    * Immutable added as a dependency
    
    * Post and PostList reducer now using Immutable.js
    
    - tests updated
    - chai and chai-http moved to devDeps

--- test/post_spec.js
+++ test/post_spec.js
@@ -1,59 +1,52 @@
-import { expect } from 'chai'
+import chai, { expect } from 'chai'
+import chaiImmutable from 'chai-immutable'
 import * as types from '../common/constants'
 import reducer from '../common/routes/Post/reducer'
+import { Map } from 'immutable'
 
+chai.use(chaiImmutable)
 // Remove this
 import fakeDB from '../server/fakeDB.js'
 
 describe('Post Reducer', () => {
+  const initialState = Map({
+    lastFetched: null,
+    isLoading: false,
+    error: null,
+    title: '',
+    content: ''
+  })
 
   it('should return default state if action is undefined', () => {
-    const initialState = []
     const nextState = reducer(initialState, 'BLAH')
     expect(nextState).to.deep.equal(initialState)
   })
 
   it('should handle LOAD_POST_REQUEST', () => {
-    const initialState = {
-      lastFetched: null,
-      isLoading: false,
-      error: null,
-      title: '',
-      content: ''
-    }
-
     const action = {
       type: types.LOAD_POST_REQUEST
     }
 
-    const nextState = reducer(initialState, action)
-    expect(nextState).to.deep.equal({
+    const expectedNextState = Map({
       lastFetched: null,
       isLoading: true,
       error: null,
       title: '',
       content: ''
     })
+
+    const nextState = reducer(initialState, action)
+    expect(nextState).to.deep.equal(expectedNextState)
   })
 
   it('should handle LOAD_POST_SUCCESS', () => {
-    const initialState = {
-      lastFetched: null,
-      isLoading: false,
-      error: null,
-      title: '',
-      content: ''
-    }
-
     const post = {
       id: '128sd043hd',
       title: 'Cloth Talk Part I',
       slug: 'cloth-talk-part-i',
       content: 'Khaled Ipsum is a major key to success.'
     }
-
     const currentTime = Date.now()
-
     const action = {
       type: types.LOAD_POST_SUCCESS,
       payload: post,
@@ -62,37 +55,35 @@ describe('Post Reducer', () => {
       }
     }
 
-    const nextState = reducer(initialState, action)
-    expect(nextState).to.deep.equal({
+    const expectedNextState = Map({
       lastFetched: currentTime,
       isLoading: false,
       error: null,
       title: 'Cloth Talk Part I',
       content: 'Khaled Ipsum is a major key to success.'
     })
+
+    const nextState = reducer(initialState, action)
+    expect(nextState).to.deep.equal(expectedNextState)
   })
 
   it('should handle LOAD_POST_FAILURE', () => {
-    const initialState = {
-      lastFetched: null,
-      isLoading: false,
-      error: null,
-      title: '',
-      content: ''
-    }
     const error = new Error('Invalid request')
     const action = {
       type: types.LOAD_POST_FAILURE,
       payload: error,
       error: true
     }
-    const nextState = reducer(initialState, action)
-    expect(nextState).to.deep.equal({
+
+    const expectedNextState = Map({
       lastFetched: null,
       isLoading: false,
       error: error,
       title: '',
       content: ''
     })
+
+    const nextState = reducer(initialState, action)
+    expect(nextState).to.deep.equal(expectedNextState)
   })
 })
