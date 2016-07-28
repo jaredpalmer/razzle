85c464c3b58e04b7019d652f92bdaa51e3ac6b44
Jared Palmer <jaredpalmer>
2016-07-27 20:52:02 -0400

    Improved DX
    Bump react to v15.2.1. Shamelessly rip the the best parts of
    create-react-app repo DX (console, open, and pretty errors).

--- tools/webpack.base.js
+++ tools/webpack.base.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   CLIENT_ENTRY:  path.join(process.cwd(), 'client'),
-  CLIENT_OUTPUT: path.join(process.cwd(), 'build'),
+  CLIENT_OUTPUT: path.join(process.cwd(), 'public/assets'),
   SERVER_ENTRY:  path.join(process.cwd(), 'server/server.js'),
   SERVER_OUTPUT: path.join(process.cwd(), 'build'),
   PUBLIC_PATH: '/'
