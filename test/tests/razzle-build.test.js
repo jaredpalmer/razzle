21ddea9590ddb5d9b54c2a1d661fad912e337b24
Jared Palmer <jared@palmer.net>
2017-05-19 15:53:00 -0400

    Add e2e tests. #272

---
+++ test/tests/razzle-build.test.js
@@ -0,0 +1,33 @@
+const shell = require('shelljs');
+const util = require('../fixtures/util');
+
+shell.config.silent = true;
+
+const stageName = 'stage-build';
+
+describe('razzle build', () => {
+  it('should compile files into a build directory', () => {
+    util.setupStageWithFixture(stageName, 'build-default');
+    const output = shell.exec('npm run build');
+    expect(shell.test('-f', 'build/assets.json')).toBe(true);
+    expect(shell.test('-f', 'build/server.js')).toBe(true);
+
+    // Should copy static assets from src/public directory
+    expect(shell.test('-f', 'build/public/nothing.txt')).toBe(true);
+
+    // Should compile client bundle to js directory
+    expect(shell.test('-d', 'build/public/static/js')).toBe(true);
+
+    expect(output.code).toBe(0);
+  });
+
+  it('should respect a custom BABEL_ENV', () => {
+    util.setupStageWithFixture(stageName, 'build-with-custom-babelrc');
+    const output = shell.exec('BABEL_ENV=custom npm run build');
+    expect(output.code).toBe(0);
+  });
+
+  afterEach(() => {
+    util.teardownStage(stageName);
+  });
+});
