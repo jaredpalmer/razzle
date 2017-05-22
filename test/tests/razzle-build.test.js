77439c08b8702d90ea66767194deb66fe5f8e186
Jared Palmer <jared@palmer.net>
2017-05-22 12:07:13 -0400

    Add env var, webpack examples and tests. Update .gitignore for .env*.local

--- test/tests/razzle-build.test.js
+++ test/tests/razzle-build.test.js
@@ -1,7 +1,7 @@
 const shell = require('shelljs');
 const util = require('../fixtures/util');
 
-// shell.config.silent = true;
+shell.config.silent = true;
 
 const stageName = 'stage-build';
 
@@ -14,6 +14,45 @@ describe('razzle build', () => {
 
     // Create server.js
     expect(shell.test('-f', 'build/server.js')).toBe(true);
+    expect(shell.test('-f', 'build/server.js.map')).toBe(true);
+
+    // Should copy static assets from src/public directory
+    expect(shell.test('-f', 'build/public/nothing.txt')).toBe(true);
+
+    // Should compile client bundle to js directory
+    expect(shell.test('-d', 'build/public/static/js')).toBe(true);
+
+    expect(output.code).toBe(0);
+  });
+
+  it('should compile files with a custom .babelrc', () => {
+    util.setupStageWithFixture(stageName, 'build-with-babelrc');
+    const output = shell.exec('yarn run build');
+    // Create asset manifest
+    expect(shell.test('-f', 'build/assets.json')).toBe(true);
+
+    // Create server.js
+    expect(shell.test('-f', 'build/server.js')).toBe(true);
+    expect(shell.test('-f', 'build/server.js.map')).toBe(true);
+
+    // Should copy static assets from src/public directory
+    expect(shell.test('-f', 'build/public/nothing.txt')).toBe(true);
+
+    // Should compile client bundle to js directory
+    expect(shell.test('-d', 'build/public/static/js')).toBe(true);
+
+    expect(output.code).toBe(0);
+  });
+
+  it('should compile files with a custom razzle.config.js', () => {
+    util.setupStageWithFixture(stageName, 'build-with-custom-config');
+    const output = shell.exec('yarn run build');
+    // Create asset manifest
+    expect(shell.test('-f', 'build/assets.json')).toBe(true);
+
+    // We modify the default server output filename -> custom.js
+    expect(shell.test('-f', 'build/custom.js')).toBe(true);
+    expect(shell.test('-f', 'build/custom.js.map')).toBe(true);
 
     // Should copy static assets from src/public directory
     expect(shell.test('-f', 'build/public/nothing.txt')).toBe(true);
