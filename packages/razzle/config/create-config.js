c8df458b3756cd88053142d12241db89d1f489f9
Jared Palmer <jaredloganpalmer@gmail.com>
2017-06-07 13:42:34 -0400

    Fix #290. Add client sourcemaps in prod (#293)

--- packages/razzle/config/create-config.js
+++ packages/razzle/config/create-config.js
@@ -332,10 +332,18 @@ module.exports = (
         // Uglify/compress and optimize our JS for production, screw ie8 when
         // possible, React only works > ie9 anyway
         new webpack.optimize.UglifyJsPlugin({
-          compress: { screw_ie8: true, warnings: false },
-          mangle: { screw_ie8: true },
-          output: { comments: false, screw_ie8: true },
-          sourceMap: false,
+          compress: {
+            warnings: false,
+            // Disabled because of an issue with Uglify breaking seemingly valid code:
+            // https://github.com/facebookincubator/create-react-app/issues/2376
+            // Pending further investigation:
+            // https://github.com/mishoo/UglifyJS2/issues/2011
+            comparisons: false,
+          },
+          output: {
+            comments: false,
+          },
+          sourceMap: true,
         }),
         // Extract our CSS into a files.
         new ExtractTextPlugin({
