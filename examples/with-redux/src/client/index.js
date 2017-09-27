da26a832a55cf78feed588b0170dc31bd782b4aa
Nicolas Cisco <ncis20@gmail.com>
2017-09-26 22:44:49 -0300

    Updated redux example

--- examples/with-redux/src/client/index.js
+++ examples/with-redux/src/client/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { hydrate } from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from '../common/store/configureStore';
 import App from '../common/containers/App';
 
 const store = configureStore(window.__PRELOADED_STATE__);
 
-render(
+hydrate(
   <Provider store={store}>
     <App />
   </Provider>,
