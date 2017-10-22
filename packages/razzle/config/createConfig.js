185a1a806c4173bbf031294dcbac11ab399a3bb4
Glenn Reyes <glenn@glennreyes.com>
2017-10-23 00:39:02 +0200

    Prevent creating multiple server chunks

--- packages/razzle/config/createConfig.js
+++ packages/razzle/config/createConfig.js
@@ -259,6 +259,10 @@ module.exports = (
       new webpack.NamedModulesPlugin(),
       // We define environment variables that can be accessed globally in our
       new webpack.DefinePlugin(dotenv.stringified),
+      // Prevent creating multiple chunks for the server
+      new webpack.optimize.LimitChunkCountPlugin({
+        maxChunks: 1,
+      }),
     ];
 
     config.entry = [paths.appServerIndexJs];
