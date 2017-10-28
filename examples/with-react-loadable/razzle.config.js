9c5c449cb5b15bea81da9a2b9beba8f02aae7720
Glenn Reyes <glenn@glennreyes.com>
2017-10-28 12:06:25 +0200

    Make config more readable

--- examples/with-react-loadable/razzle.config.js
+++ examples/with-react-loadable/razzle.config.js
@@ -1,16 +1,19 @@
 const { ReactLoadablePlugin } = require('react-loadable/webpack');
 
 module.exports = {
-  modify: (config, { target }) =>
-    target === 'web'
-      ? {
-          ...config,
-          plugins: [
-            ...config.plugins,
-            new ReactLoadablePlugin({
-              filename: './build/react-loadable.json',
-            }),
-          ],
-        }
-      : config,
+  modify: (config, { target }) => {
+    if (target === 'web') {
+      return {
+        ...config,
+        plugins: [
+          ...config.plugins,
+          new ReactLoadablePlugin({
+            filename: './build/react-loadable.json',
+          }),
+        ],
+      };
+    }
+
+    return config;
+  },
 };
