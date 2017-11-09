830c877ea2cad8496e32d3590c0471fd3cc0eea4
eliot-akira <me@eliotakira.com>
2017-11-09 13:59:17 +0100

    Remove unused/unnecessary lines

--- examples/with-scss/razzle.config.js
+++ examples/with-scss/razzle.config.js
@@ -1,6 +1,5 @@
 // Based on: https://gist.github.com/jaredpalmer/0a91a7bd354b875b913c74f4b16125f7
 
-const path = require('path')
 const autoprefixer = require('autoprefixer')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
@@ -39,7 +38,6 @@ module.exports = {
         {
           loader: 'css-loader',
           options: {
-            modules: false,
             sourceMap: true,
           },
         },
