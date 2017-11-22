dc24c395975800faaf55e61b22f5ddfa0d022275
Jolyon Russ <jolyonruss@gmail.com>
2017-11-22 10:25:22 -0800

    Adding StyledComponents example (#398)
    * duplicated basic example
    
    * Added StyledComponents implementation
    
    * Removed first person

---
+++ examples/with-styled-components/README.md
@@ -0,0 +1,24 @@
+# Razzle Basic Example
+
+## How to use
+Download the example [or clone the whole project](https://github.com/jaredpalmer/razzle.git):
+
+```bash
+curl https://codeload.github.com/jaredpalmer/razzle/tar.gz/master | tar -xz --strip=2 razzle-master/examples/basic
+cd basic
+```
+
+Install it and run:
+
+```bash
+yarn install
+yarn start
+```
+
+## Idea behind the example
+This is a basic, bare-bones example that shows a very minimal implementation
+of [StyledComponents](https://github.com/styled-components/styled-components).
+It satisfies the entry points `src/index.js` for the server and`src/client.js`
+for the browser.
+There are comments in `src/server.js` to show how the styles are gathered and
+rendered into to the DOM
---
+++ examples/with-styled-components/src/server.js
@@ -0,0 +1,51 @@
+import App from './App';
+import React from 'react';
+import express from 'express';
+import { renderToString } from 'react-dom/server';
+// Import the StyledComponents SSR util
+import { ServerStyleSheet } from 'styled-components';
+
+const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
+
+const server = express();
+
+server
+  .disable('x-powered-by')
+  .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
+  .get('/*', (req, res) => {
+    // Create the server side style sheet
+    const sheet = new ServerStyleSheet();
+    // When the app is rendered collect the styles that are used inside it
+    const markup = renderToString(sheet.collectStyles(<App />));
+    // Generate all the style tags so they can be rendered into the page
+    const styleTags = sheet.getStyleTags();
+
+    res.send(
+      `<!doctype html>
+    <html lang="">
+    <head>
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta charSet='utf-8' />
+        <title>Welcome to Razzle</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        ${
+          assets.client.css
+            ? `<link rel="stylesheet" href="${assets.client.css}">`
+            : ''
+        }
+        ${
+          process.env.NODE_ENV === 'production'
+            ? `<script src="${assets.client.js}" defer></script>`
+            : `<script src="${assets.client.js}" defer crossorigin></script>`
+        }
+        <!-- Render the style tags gathered from the components into the DOM -->
+        ${styleTags}
+    </head>
+    <body>
+        <div id="root">${markup}</div>
+    </body>
+</html>`
+    );
+  });
+
+export default server;
