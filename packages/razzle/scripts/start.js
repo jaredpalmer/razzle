d81e7f2c1b407e40927d11d8030a6a242bc388f4
Guy Brukhis <guy.br7@gmail.com>
2017-11-29 13:42:40 +0200

    Check if ports in use in start script (#397)

--- packages/razzle/scripts/start.js
+++ packages/razzle/scripts/start.js
@@ -10,6 +10,7 @@ const devServer = require('webpack-dev-server');
 const printErrors = require('razzle-dev-utils/printErrors');
 const clearConsole = require('react-dev-utils/clearConsole');
 const logger = require('razzle-dev-utils/logger');
+const setPorts = require('razzle-dev-utils/setPorts');
 
 process.noDeprecation = true; // turns off that loadQuery clutter.
 
@@ -17,74 +18,76 @@ if (process.argv.includes('--inspect')) {
   process.env.INSPECT_ENABLED = true;
 }
 
-// Optimistically, we make the console look exactly like the output of our
-// FriendlyErrorsPlugin during compilation, so the user has immediate feedback.
-// clearConsole();
-logger.start('Compiling...');
-let razzle = {};
+function main() {
+  // Optimistically, we make the console look exactly like the output of our
+  // FriendlyErrorsPlugin during compilation, so the user has immediate feedback.
+  // clearConsole();
+  logger.start('Compiling...');
+  let razzle = {};
 
-// Check for razzle.config.js file
-if (fs.existsSync(paths.appRazzleConfig)) {
-  try {
-    razzle = require(paths.appRazzleConfig);
-  } catch (e) {
-    clearConsole();
-    logger.error('Invalid razzle.config.js file.', e);
-    process.exit(1);
-  }
-}
-
-// Delete assets.json to always have a manifest up to date
-fs.removeSync(paths.appManifest);
-
-// Create dev configs using our config factory, passing in razzle file as
-// options.
-let clientConfig = createConfig('web', 'dev', razzle);
-let serverConfig = createConfig('node', 'dev', razzle);
-
-// Check if razzle.config has a modify function. If it does, call it on the
-// configs we just created.
-if (razzle.modify) {
-  clientConfig = razzle.modify(
-    clientConfig,
-    { target: 'web', dev: true },
-    webpack
-  );
-  serverConfig = razzle.modify(
-    serverConfig,
-    { target: 'node', dev: true },
-    webpack
-  );
-}
-
-const serverCompiler = compile(serverConfig);
-
-// Start our server webpack instance in watch mode.
-serverCompiler.watch(
-  {
-    quiet: true,
-    stats: 'none',
-  },
-  /* eslint-disable no-unused-vars */
-  stats => {}
-);
-
-// Compile our assets with webpack
-const clientCompiler = compile(clientConfig);
-
-// Create a new instance of Webpack-dev-server for our client assets.
-// This will actually run on a different port than the users app.
-const clientDevServer = new devServer(clientCompiler, clientConfig.devServer);
-
-// Start Webpack-dev-server
-clientDevServer.listen(
-  (process.env.PORT && parseInt(process.env.PORT) + 1) || razzle.port || 3001,
-  err => {
-    if (err) {
-      logger.error(err);
+  // Check for razzle.config.js file
+  if (fs.existsSync(paths.appRazzleConfig)) {
+    try {
+      razzle = require(paths.appRazzleConfig);
+    } catch (e) {
+      clearConsole();
+      logger.error('Invalid razzle.config.js file.', e);
+      process.exit(1);
     }
   }
-);
+
+  // Delete assets.json to always have a manifest up to date
+  fs.removeSync(paths.appManifest);
+
+  // Create dev configs using our config factory, passing in razzle file as
+  // options.
+  let clientConfig = createConfig('web', 'dev', razzle);
+  let serverConfig = createConfig('node', 'dev', razzle);
+
+  // Check if razzle.config has a modify function. If it does, call it on the
+  // configs we just created.
+  if (razzle.modify) {
+    clientConfig = razzle.modify(
+      clientConfig,
+      { target: 'web', dev: true },
+      webpack
+    );
+    serverConfig = razzle.modify(
+      serverConfig,
+      { target: 'node', dev: true },
+      webpack
+    );
+  }
+
+  const serverCompiler = compile(serverConfig);
+
+  // Start our server webpack instance in watch mode.
+  serverCompiler.watch(
+    {
+      quiet: true,
+      stats: 'none',
+    },
+    /* eslint-disable no-unused-vars */
+    stats => {}
+  );
+
+  // Compile our assets with webpack
+  const clientCompiler = compile(clientConfig);
+
+  // Create a new instance of Webpack-dev-server for our client assets.
+  // This will actually run on a different port than the users app.
+  const clientDevServer = new devServer(clientCompiler, clientConfig.devServer);
+
+  // Start Webpack-dev-server
+  clientDevServer.listen(
+    (process.env.PORT && parseInt(process.env.PORT) + 1) || razzle.port || 3001,
+    err => {
+      if (err) {
+        logger.error(err);
+      }
+    }
+  );
+}
 
 // Webpack compile in a try-catch
 function compile(config) {
@@ -97,3 +100,7 @@ function compile(config) {
   }
   return compiler;
 }
+
+setPorts()
+  .then(main)
+  .catch(console.error);
