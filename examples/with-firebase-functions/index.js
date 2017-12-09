bee82cd611b520536997198b246791e2b09c31d1
Thee Sritabtim <thammada@gmail.com>
2017-12-09 18:45:48 +0700

    added Firebase functions example

---
+++ examples/with-firebase-functions/index.js
@@ -0,0 +1,5 @@
+'use strict'
+const functions = require('firebase-functions');
+const app = require('./server/build/server.bundle.js').default;
+
+exports.app = functions.https.onRequest(app)
---
+++ examples/with-firebase-functions/razzle.config.js
@@ -0,0 +1,16 @@
+'use strict';
+const path = require('path');
+module.exports = {
+  modify(config, { target, dev }) {
+    const appConfig = config; // stay immutable here
+
+    if (target === 'node' && !dev) {
+      appConfig.entry = path.resolve(__dirname, './src/server.js');
+      appConfig.output.filename = 'server.bundle.js';
+      appConfig.output.path = path.resolve(__dirname, './server/build');
+      appConfig.output.libraryTarget = 'commonjs2';
+    }
+
+    return appConfig;
+  },
+}
