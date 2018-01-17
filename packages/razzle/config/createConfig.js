36819345f6b9758d54a08fb4093e082f006a454a
Zim <roleg2@gmail.com>
2018-01-17 16:11:06 +0200

    Minimize css on production build

--- packages/razzle/config/createConfig.js
+++ packages/razzle/config/createConfig.js
@@ -203,6 +203,7 @@ module.exports = (
                       loader: require.resolve('css-loader'),
                       options: {
                         importLoaders: 1,
+                        minimize: true
                       },
                     },
                     {
