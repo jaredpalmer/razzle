151be50b62136a1391974f3b984cc48310d72d77
Jared Palmer <jared@palmer.net>
2018-02-08 19:57:18 -0500

    Fix after.js example

--- examples/with-afterjs/src/Home.js
+++ examples/with-afterjs/src/Home.js
@@ -4,12 +4,11 @@ import './Home.css';
 import { Link } from 'react-router-dom';
 
 class Home extends Component {
-  static getInitialProps({ req, res, match, history, location, ...ctx }) {
-    console.log(ctx);
-    return Promise.resolve(ctx);
+  static async getInitialProps({ req, res, match, history, location, ...ctx }) {
+    return { whatever: 'stuff' };
   }
+
   render() {
-    console.log(this.props);
     return (
       <div className="Home">
         <div className="Home-header">
@@ -17,9 +16,8 @@ class Home extends Component {
           <h2>Welcome to After.js</h2>
         </div>
         <p className="Home-intro">
-          To get started, edit
-          <code>src/Home.js</code> or <code>src/About.js</code>and save to
-          reload.
+          To get started, edit <code>src/Home.js</code> or{' '}
+          <code>src/About.js</code>and save to reload.
         </p>
         <Link to="/about">About -></Link>
       </div>
