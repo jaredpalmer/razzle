986e6c4016d5def55407f5141d4fb70811edb0c8
Jared Palmer <jared@palmer.net>
2018-02-14 10:44:17 -0500

    Close #488. Add react-native-web-example

---
+++ examples/with-react-native-web/razzle.config.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  modify(config, { target, dev }, webpack) {
+    // Since RN web takes care of CSS, we should remove it for a #perf boost
+    config.module.rules = config.module.rules
+      .filter(
+        rule =>
+          !(rule.test && rule.test.exec && rule.test.exec('./somthing.css'))
+      )
+      .filter(
+        rule =>
+          !(
+            rule.test &&
+            rule.test.exec &&
+            rule.test.exec('./somthing.module.css')
+          )
+      );
+
+    const extPlugin = require(require.resolve('extract-text-webpack-plugin'));
+    config.plugins = config.plugins.filter(w => !(w instanceof extPlugin));
+
+    return config;
+  },
+};
---
+++ examples/with-react-native-web/src/server.js
@@ -0,0 +1,47 @@
+import App from './App';
+
+import express from 'express';
+import ReactDOMServer from 'react-dom/server';
+import { AppRegistry } from 'react-native';
+
+const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
+
+const server = express();
+
+server
+  .disable('x-powered-by')
+  .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
+  .get('/*', (req, res) => {
+    // register the app
+    AppRegistry.registerComponent('App', () => App);
+
+    // prerender the app
+    const { element, getStyleElement } = AppRegistry.getApplication('App', {});
+    // first the element
+    const html = ReactDOMServer.renderToString(element);
+    // then the styles
+    const css = ReactDOMServer.renderToStaticMarkup(getStyleElement());
+
+    res.send(
+      `<!doctype html>
+    <html lang="">
+    <head>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta charSet='utf-8' />
+        <title>Welcome to Razzle</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        ${css}
+        ${
+          process.env.NODE_ENV === 'production'
+            ? `<script src="${assets.client.js}" defer></script>`
+            : `<script src="${assets.client.js}" defer crossorigin></script>`
+        }
+    </head>
+    <body>
+        <div id="root">${html}</div>
+    </body>
+</html>`
+    );
+  });
+
+export default server;
