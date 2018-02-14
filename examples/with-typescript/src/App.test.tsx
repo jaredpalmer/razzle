d11b30b59799bbd5da4f1cc6f0426c217d72fed3
Jake Cooper <jake.elijah.cooper@gmail.com>
2018-02-13 19:27:11 -0800

    Added Full TSLint to TypeScript example

--- examples/with-typescript/src/App.test.tsx
+++ examples/with-typescript/src/App.test.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 import App from './App';
+
 import { MemoryRouter } from 'react-router-dom';
 
 describe('<App />', () => {
--- examples/with-typescript/src/App.tsx
+++ examples/with-typescript/src/App.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './Home';
+
 import './App.css';
 
 const App = () => (
