e689fa2570d7009f3fab09758ff8ef4ddc8e3ba1
William Esz <flagaello@gmail.com>
2018-02-19 03:04:56 +0100

    Update createJestConfig to match CRA's

--- packages/razzle/config/createJestConfig.js
+++ packages/razzle/config/createJestConfig.js
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 'use strict';
 
 const fs = require('fs');
@@ -30,14 +36,28 @@ module.exports = (resolve, rootDir) => {
         'config/jest/fileTransform.js'
       ),
     },
-    transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs)$'],
+    transformIgnorePatterns: [
+      '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs)$',
+      '^.+\\.module\\.css$',
+    ],
     moduleNameMapper: {
       '^react-native$': 'react-native-web',
+      '^.+\\.module\\.css$': 'identity-obj-proxy',
     },
+    moduleFileExtensions: [
+      'web.js',
+      'mjs',
+      'js',
+      'json',
+      'web.jsx',
+      'jsx',
+      'node',
+    ],
   };
   if (rootDir) {
     config.rootDir = rootDir;
   }
+
   const overrides = Object.assign({}, require(paths.appPackageJson).jest);
   const supportedKeys = [
     'collectCoverageFrom',
@@ -47,12 +67,15 @@ module.exports = (resolve, rootDir) => {
     'moduleFileExtensions',
     'moduleNameMapper',
     'modulePaths',
-    'snapshotSerializers',
+    'resetMocks',
+    'resetModules',
     'setupFiles',
+    'snapshotSerializers',
     'testMatch',
     'testResultsProcessor',
     'transform',
     'transformIgnorePatterns',
+    'watchPathIgnorePatterns',
   ];
   if (overrides) {
     supportedKeys.forEach(key => {
@@ -63,17 +86,44 @@ module.exports = (resolve, rootDir) => {
     });
     const unsupportedKeys = Object.keys(overrides);
     if (unsupportedKeys.length) {
-      console.error(
-        chalk.red(
-          'Out of the box, Razzle only supports overriding ' +
-            'these Jest options:\n\n' +
-            supportedKeys.map(key => chalk.bold('  \u2022 ' + key)).join('\n') +
-            '.\n\n' +
-            'These options in your package.json Jest configuration ' +
-            'are not currently supported by Razzle:\n\n' +
-            unsupportedKeys.map(key => chalk.bold('  \u2022 ' + key)).join('\n')
-        )
-      );
+      const isOverridingSetupFile =
+        unsupportedKeys.indexOf('setupTestFrameworkScriptFile') > -1;
+
+      if (isOverridingSetupFile) {
+        console.error(
+          chalk.red(
+            'We detected ' +
+              chalk.bold('setupTestFrameworkScriptFile') +
+              ' in your package.json.\n\n' +
+              'Remove it from Jest configuration, and put the initialization code in ' +
+              chalk.bold('src/setupTests.js') +
+              '.\nThis file will be loaded automatically.\n'
+          )
+        );
+      } else {
+        console.error(
+          chalk.red(
+            '\nOut of the box, Razzle only supports overriding ' +
+              'these Jest options:\n\n' +
+              supportedKeys
+                .map(key => chalk.bold('  \u2022 ' + key))
+                .join('\n') +
+              '.\n\n' +
+              'These options in your package.json Jest configuration ' +
+              'are not currently supported by Razzle:\n\n' +
+              unsupportedKeys
+                .map(key => chalk.bold('  \u2022 ' + key))
+                .join('\n') +
+              '\n\nIf you wish to override other Jest options, you need to ' +
+              'eject from the default setup. You can do so by running ' +
+              chalk.bold('npm run eject') +
+              ' but remember that this is a one-way operation. ' +
+              'You may also file an issue with Razzle to discuss ' +
+              'supporting more options out of the box.\n'
+          )
+        );
+      }
+
       process.exit(1);
     }
   }
