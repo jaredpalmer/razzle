d688fe120a964eea3e2a43d9b3a4d644dd83ae9a
소재경 <worudso@hplusmall.com>
2018-05-02 19:11:06 +0900

    Add typescript afterjs example

---
+++ examples/with-typescript-afterjs/src/routes.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { asyncComponent } from '@jaredpalmer/after';
+
+export default [
+  {
+    path: '/',
+    exact: true,
+    component: asyncComponent({
+      loader: () => import('./Home'), // required
+      Placeholder: () => <div>...LOADING...</div>, // this is optional, just returns null by default
+    }),
+  },
+  {
+    path: '/about',
+    exact: true,
+    component: asyncComponent({
+      loader: () => import('./About'), // required
+      Placeholder: () => <div>...LOADING...</div>, // this is optional, just returns null by default
+    }),
+  },
+];
