47a380cfc06a28ccb55c63169dd093906db117f5
Ray Andrew <raydreww@gmail.com>
2018-06-17 17:32:24 +0700

    add scss plugin and fixes to makeLoaderFinder

--- packages/razzle-plugin-scss/index.js
+++ packages/razzle-plugin-scss/index.js
@@ -1,8 +1,82 @@
 'use strict';
 
-module.exports = function modify(config) {
-  console.log(
-    'Not yet implemented. Help out at https://github.com/jaredpalmer/razzle'
-  );
+const autoprefixer = require('autoprefixer');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const PostCssFlexBugFixes = require('postcss-flexbugs-fixes');
+const paths = require('razzle/config/paths');
+
+module.exports = (defaultConfig, env) => {
+  const { target, dev } = env;
+  const isServer = target !== 'web';
+
+  const config = Object.assign({}, defaultConfig);
+
+  const postCssLoader = {
+    loader: require.resolve('postcss-loader'),
+    options: {
+      ident: 'postcss',
+      sourceMap: true,
+      plugins: () => [
+        PostCssFlexBugFixes,
+        autoprefixer({
+          browsers: ['>1%', 'last 4 versions', 'Firefox ESR', 'not ie < 9'],
+          flexbox: 'no-2009',
+        }),
+      ],
+    },
+  };
+
+  const sassLoader = {
+    loader: require.resolve('sass-loader'),
+    options: {
+      includePaths: [paths.appNodeModules],
+    },
+  };
+
+  config.module.rules = [
+    ...config.module.rules,
+    {
+      test: /\.(sa|sc|c)ss$/,
+      use: isServer
+        ? [
+            {
+              loader: require.resolve('css-loader/locals'),
+              options: {
+                importLoaders: 1,
+              },
+            },
+          ]
+        : dev
+          ? [
+              require.resolve('style-loader'),
+              {
+                loader: require.resolve('css-loader'),
+                options: {
+                  importLoaders: 1,
+                  modules: false,
+                  sourceMap: true,
+                },
+              },
+              require.resolve('resolve-url-loader'),
+              postCssLoader,
+              sassLoader,
+            ]
+          : [
+              MiniCssExtractPlugin.loader,
+              {
+                loader: require.resolve('css-loader'),
+                options: {
+                  importLoaders: 1,
+                  modules: false,
+                  minimize: true,
+                },
+              },
+              require.resolve('resolve-url-loader'),
+              postCssLoader,
+              sassLoader,
+            ],
+    },
+  ];
+
   return config;
 };
