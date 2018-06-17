cac67a4bf99531122d5bb37b754793499fce2e9e
Ray Andrew <raydreww@gmail.com>
2018-06-17 17:32:24 +0700

    add scss plugin and fixes to makeLoaderFinder

---
+++ examples/with-scss/src/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+
+import React from 'react';
+
+const App = () => (
+  <div>
+    <div>Welcome to Razzle.</div>
+    <div className="info">Hot reload enabled!!</div>
+  </div>
+);
+
+export default App;
