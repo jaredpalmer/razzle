7a030220dd3ce320a3fc9172af4f0f2f1c0f1841
rhodee <info@rhodee.us>
2018-06-19 08:10:19 -0400

    Add with-pwa example

---
+++ examples/with-pwa/src/index.js
@@ -0,0 +1,31 @@
+import app from './server';
+import http from 'http';
+import { unregister } from './registerServiceWorker'
+
+const server = http.createServer(app);
+
+let currentApp = app;
+
+server.listen(process.env.PORT || 3000, error => {
+  if (error) {
+    console.log(error);
+  }
+
+  console.log('🚀 started');
+});
+
+if (module.hot) {
+  console.log('✅  Server-side HMR Enabled!');
+
+  module.hot.accept('./server', () => {
+    console.log('🔁  HMR Reloading `./server`...');
+    server.removeListener('request', currentApp);
+    const newApp = require('./server').default;
+    server.on('request', newApp);
+    currentApp = newApp;
+  });
+}
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/2vJdu84
+unregister();
