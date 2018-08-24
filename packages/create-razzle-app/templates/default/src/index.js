c8b55bebd5c4dadb6953dd3e8d94fac084c854d4
Bram Zijp <bramzijp1@gmail.com>
2018-08-24 18:41:30 +0200

    Fix for '[HMR] You need to restart the application!' on server sided err

--- packages/create-razzle-app/templates/default/src/index.js
+++ packages/create-razzle-app/templates/default/src/index.js
@@ -16,11 +16,23 @@ server.listen(process.env.PORT || 3000, error => {
 if (module.hot) {
   console.log('✅  Server-side HMR Enabled!');
 
+  const noErrors = () => {
+    try {
+      require('./server').default;
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+    return true;
+  };
+
   module.hot.accept('./server', () => {
-    console.log('🔁  HMR Reloading `./server`...');
-    server.removeListener('request', currentApp);
-    const newApp = require('./server').default;
-    server.on('request', newApp);
-    currentApp = newApp;
+    if (noErrors()) {
+      console.log('🔁  HMR Reloading `./server`...');
+      server.removeListener('request', currentApp);
+      const newApp = require('./server').default;
+      server.on('request', newApp);
+      currentApp = newApp;
+    }
   });
 }
