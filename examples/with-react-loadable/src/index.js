560a9f490bdcac89b29a03b7d9c471ff21ff6619
Haja Rasoahaingo <haja.rasoah@gmail.com>
2018-09-12 03:26:07 +0200

    Fix HMR : client was not working + mismatch between server/client (#749)

--- examples/with-react-loadable/src/index.js
+++ examples/with-react-loadable/src/index.js
@@ -13,10 +13,11 @@ Loadable.preloadAll().then(() => {
 if (module.hot) {
   console.log('✅  Server-side HMR Enabled!');
 
-  module.hot.accept('./server', () => {
+  module.hot.accept('./server', async () => {
     console.log('🔁  HMR Reloading `./server`...');
     server.removeListener('request', currentApp);
     const newApp = require('./server').default;
+    await Loadable.preloadAll();
     server.on('request', newApp);
     currentApp = newApp;
   });
