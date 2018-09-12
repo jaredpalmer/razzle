50d428f6a642f1c9ce70bf783bf291e8e29ef7c5
Haja Rasoahaingo <haja.rasoah@gmail.com>
2018-09-12 03:25:37 +0200

    Update css chunks: use [name] instead of [id] (#750)

--- packages/razzle/config/createConfig.js
+++ packages/razzle/config/createConfig.js
@@ -495,6 +495,7 @@ module.exports = (
         // Extract our CSS into a files.
         new MiniCssExtractPlugin({
           filename: 'static/css/bundle.[contenthash:8].css',
+          chunkFilename: 'static/css/[name].[contenthash:8].chunk.css',
           // allChunks: true because we want all css to be included in the main
           // css bundle when doing code splitting to avoid FOUC:
           // https://github.com/facebook/create-react-app/issues/2415
