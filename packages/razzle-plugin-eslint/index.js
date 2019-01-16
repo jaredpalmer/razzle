e4b276dbb6584c6881e2c4bb8f88100d4bbef722
Bart Langelaan <langelaanbart@gmail.com>
2019-01-16 17:42:53 +0100

    Move eslint specific code over to razzle-plugin-eslint

--- packages/razzle-plugin-eslint/index.js
+++ packages/razzle-plugin-eslint/index.js
@@ -1,8 +1,28 @@
 'use strict';
 
+const eslintFormatter = require('react-dev-utils/eslintFormatter');
+
 module.exports = function modify(config) {
-  console.log(
-    'Not yet implemented. Help out at https://github.com/jaredpalmer/razzle'
-  );
+  const mainEslintOptions = {
+    baseConfig: {
+      extends: [require.resolve('eslint-config-react-app')],
+    },
+    formatter: eslintFormatter,
+    eslintPath: require.resolve('eslint'),
+    ignore: false,
+    useEslintrc: true,
+  };
+
+  config.module.rules = [
+    {
+      test: /\.(js|jsx|mjs)$/,
+      enforce: 'pre',
+      loader: require.resolve('eslint-loader'),
+      options: mainEslintOptions,
+      exclude: /node_modules/,
+    },
+    ...config.module.rules,
+  ];
+
   return config;
 };
