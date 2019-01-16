e4fd14e9911b7751433bf2540b13a25e76f62c2a
Bart Langelaan <langelaanbart@gmail.com>
2019-01-16 23:10:05 +0100

    Add a temporary eslint-plugin-react setting

--- packages/razzle-plugin-eslint/index.js
+++ packages/razzle-plugin-eslint/index.js
@@ -6,6 +6,13 @@ module.exports = function modify(config) {
   const mainEslintOptions = {
     baseConfig: {
       extends: [require.resolve('eslint-config-react-app')],
+      // This setting can be removed once this commit is released:
+      // https://github.com/facebook/create-react-app/commit/005ee5b9525d476d2eb2dfb5b8afcd15b65dd5d2
+      settings: {
+        react: {
+          version: 'detect',
+        },
+      },
     },
     formatter: eslintFormatter,
     eslintPath: require.resolve('eslint'),
