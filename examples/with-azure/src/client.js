7d7ebb8f91302371f10b9a2c021347aa6e7d139b
Carlos Ortiz <carlos.ortiz@softtek.com>
2019-02-26 13:15:20 -0600

    Initial commit

---
+++ examples/with-azure/src/client.js
@@ -0,0 +1,15 @@
+import App from './App';
+import BrowserRouter from 'react-router-dom/BrowserRouter';
+import React from 'react';
+import { hydrate } from 'react-dom';
+
+hydrate(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
+
+if (module.hot) {
+  module.hot.accept();
+}
