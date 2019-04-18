9e5efbfe250f6987a1b8a0c35151d60211f537a2
nima <nimaarefi@outlook.com>
2019-04-18 20:55:09 +0430

    allow jest watchPlugins option

--- packages/razzle/config/createJestConfig.js
+++ packages/razzle/config/createJestConfig.js
@@ -54,6 +54,7 @@ module.exports = (resolve, rootDir) => {
     'testResultsProcessor',
     'transform',
     'transformIgnorePatterns',
+    'watchPlugins',
   ];
   if (overrides) {
     supportedKeys.forEach(key => {
