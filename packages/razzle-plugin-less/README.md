36b81c8747fc2bae53ba638d0f8a17089418fe8c
Majid <anonymousmajsaj@gmail.com>
2019-08-03 13:34:45 +0430

    added less plugin

--- packages/razzle-plugin-less/README.md
+++ packages/razzle-plugin-less/README.md
@@ -1,3 +1,42 @@
 # razzle-plugin-less
 
-Not yet implemented.
+
+## Usage in Razzle Projects
+
+```bash
+yarn add razzle-plugin-less --dev
+```
+
+### With the default options
+
+```js
+// razzle.config.js
+
+module.exports = {
+  plugins: ['less'],
+};
+```
+
+---
+
+### With custom options
+
+```js
+// razzle.config.js
+
+module.exports = {
+  plugins: [
+    {
+      name: 'less',
+      options: {
+        less: {
+          dev: {
+                strictMath: true,
+                noIeCompat: true,
+            },  
+        },
+      },
+    }
+  ],
+};
+```
--- packages/razzle-plugin-less/index.js
+++ packages/razzle-plugin-less/index.js
@@ -1,8 +1,80 @@
 'use strict';
 
-module.exports = function modify(config) {
-  console.log(
-    'Not yet implemented. Help out at https://github.com/jaredpalmer/razzle'
-  );
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const paths = require('razzle/config/paths');
+
+const defaultOptions = {
+  less: {
+    dev: {
+      sourceMap: true,
+      javascriptEnabled: true,
+      paths: [paths.appNodeModules],
+    },
+    prod: {
+      sourceMap: false,
+      javascriptEnabled: true,
+      paths: [paths.appNodeModules],
+    },
+  },
+  css: {
+    dev: {
+      sourceMap: true,
+      importLoaders: 1,
+    },
+    prod: {
+      sourceMap: false,
+      importLoaders: 1,
+      minimize: true,
+    },
+  },
+  style: {},
+};
+
+module.exports = (
+  defaultConfig,
+  { target, dev },
+  webpack,
+  userOptions = {}
+) => {
+  const constantEnv = dev ? 'dev' : 'prod';
+
+  const config = Object.assign({}, defaultConfig);
+
+  const options = Object.assign({}, defaultOptions, userOptions);
+
+  const styleLoader = dev
+    ? {
+        loader: require.resolve('style-loader'),
+        options: options.style,
+      }
+    : MiniCssExtractPlugin.loader;
+
+  const cssLoader = {
+    loader: require.resolve('css-loader'),
+    options: options.css[constantEnv],
+  };
+
+  const lessLoader = {
+    loader: 'less-loader',
+    options: options.less[constantEnv],
+  };
+
+  config.module.rules = [
+    ...config.module.rules,
+    {
+      test: /\.(sa|sc)ss$/,
+      use:
+        target !== 'web'
+          ? [
+              {
+                loader: require.resolve('css-loader/locals'),
+                options: options.css,
+              },
+              lessLoader,
+            ]
+          : [styleLoader, cssLoader, lessLoader],
+    },
+  ];
+
   return config;
 };
