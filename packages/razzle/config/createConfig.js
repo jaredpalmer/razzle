da99b9f0692146221545caef6252dbea888230ce
nima <nimaarefi@outlook.com>
2019-09-23 23:09:56 +0330

    don't spilit the css!

--- packages/razzle/config/createConfig.js
+++ packages/razzle/config/createConfig.js
@@ -459,16 +459,24 @@ module.exports = (
         new MiniCssExtractPlugin({
           filename: 'static/css/bundle.[contenthash:8].css',
           chunkFilename: 'static/css/[name].[contenthash:8].chunk.css',
-          // allChunks: true because we want all css to be included in the main
-          // css bundle when doing code splitting to avoid FOUC:
-          // https://github.com/facebook/create-react-app/issues/2415
-          allChunks: true,
         }),
         new webpack.HashedModuleIdsPlugin(),
         new webpack.optimize.AggressiveMergingPlugin(),
       ];
 
       config.optimization = {
+        // because we want all css to be included in the main
+        // css bundle when doing code splitting to avoid FOUC
+        splitChunks: {
+          cacheGroups: {
+            styles: {
+              name: 'styles',
+              test: /\.(css|scss|sass|sss|less)$/,
+              chunks: 'all',
+              enforce: true,
+            },
+          },
+        },
         minimize: true,
         minimizer: [
           new TerserPlugin({
