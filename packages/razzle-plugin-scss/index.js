4e1ac6047f5a22ca0854e6c01b229271f8f83c2f
Aswin S <aswin-s@users.noreply.github.com>
2019-11-20 06:23:46 +0530

    Remove browser key from autoprefixer arguments
    browser list is being read from package.json now for interoperability with other tools

--- packages/razzle-plugin-scss/index.js
+++ packages/razzle-plugin-scss/index.js
@@ -18,7 +18,6 @@ const defaultOptions = {
     plugins: [
       PostCssFlexBugFixes,
       autoprefixer({
-        browsers: ['>1%', 'last 4 versions', 'Firefox ESR', 'not ie < 9'],
         flexbox: 'no-2009',
       }),
     ],
