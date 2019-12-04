e0f1bb1eb8d82680d820c16b102b1a807ac70f3f
Nima Arefi <nimaarefi@outlook.com>
2019-12-04 23:26:24 +0330

    change CRA to razzle

--- packages/razzle/config/modules.js
+++ packages/razzle/config/modules.js
@@ -34,7 +34,7 @@ function getAdditionalModulePaths(options = {}) {
   throw new Error(
     logger.error(
       "Your project's `baseUrl` can only be set to `src` or `node_modules`." +
-        ' Create React App does not support other values at this time.'
+        ' Razzle does not support other values at this time.'
     )
   );
 }
