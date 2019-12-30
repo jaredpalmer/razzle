7a80721286723426fe297555f251b51dda3e9691
Majid <anonymousmajsaj@gmail.com>
2019-12-30 12:13:44 +0330

    ADD less loader

--- packages/razzle-plugin-less/index.js
+++ packages/razzle-plugin-less/index.js
@@ -1,8 +1,89 @@
 'use strict';
 
-module.exports = function modify(config) {
-  console.log(
-    'Not yet implemented. Help out at https://github.com/jaredpalmer/razzle'
-  );
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const paths = require('razzle/config/paths');
+
+module.exports = (
+  defaultConfig,
+  { target, dev },
+  webpack,
+  userOptions = {}
+) => {
+  const config = defaultConfig;
+
+  const defaultOptions = {
+    css: {
+      dev: {
+        sourceMap: true,
+        importLoaders: 1,
+      },
+      prod: {
+        sourceMap: false,
+        importLoaders: 1,
+        minimize: true,
+      }
+    },
+    less: {
+      dev: {
+        sourceMap: true,
+        paths: [paths.appNodeModules],
+      },
+      prod: {
+        sourceMap: false,
+        paths: [paths.appNodeModules],
+      }
+    },
+    style: {
+      dev: {
+
+      }
+    }
+  };
+
+  // default options will overwrite with user options
+  const options = Object.assign({}, defaultOptions, userOptions);
+
+  const styleLoader = dev
+    ? {
+      loader: require.resolve('style-loader'),
+      options: options.style.dev,
+    }
+    : MiniCssExtractPlugin.loader;
+
+  const cssLoader = {
+    loader: require.resolve('css-loader'),
+    options: dev ?
+      options.css.dev :
+      options.css.prod
+  };
+
+  const lessLoader = {
+    loader: require.resolve('less-loader'),
+    options: dev ?
+      options.less.dev :
+      options.less.prod
+  };
+
+  config.module.rules = [
+    ...config.module.rules,
+    {
+      test: /\.less$/,
+      use:
+        target === 'node'
+          ? [
+            {
+              loader: require.resolve('css-loader/locals'),
+              options: options.css,
+            },
+            lessLoader,
+          ]
+          : [
+            styleLoader,
+            cssLoader,
+            lessLoader
+          ],
+    },
+  ];
+
   return config;
 };
