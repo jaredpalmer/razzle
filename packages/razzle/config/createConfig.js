be6ef101876cbf432f4bf95759d9721a76ef4b75
ambar <ambar.lee@gmail.com>
2020-03-15 05:52:29 +0800

    remove invalid option for `MiniCssExtractPlugin` (#948)

--- packages/razzle/config/createConfig.js
+++ packages/razzle/config/createConfig.js
@@ -504,14 +504,10 @@ module.exports = (
         ...config.plugins,
         // Define production environment vars
         new webpack.DefinePlugin(dotenv.stringified),
-        // Extract our CSS into a files.
+        // Extract our CSS into files.
         new MiniCssExtractPlugin({
           filename: 'static/css/bundle.[contenthash:8].css',
           chunkFilename: 'static/css/[name].[contenthash:8].chunk.css',
-          // allChunks: true because we want all css to be included in the main
-          // css bundle when doing code splitting to avoid FOUC:
-          // https://github.com/facebook/create-react-app/issues/2415
-          allChunks: true,
         }),
         new webpack.HashedModuleIdsPlugin(),
         new webpack.optimize.AggressiveMergingPlugin(),
