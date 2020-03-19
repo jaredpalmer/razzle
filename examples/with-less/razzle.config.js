af4397cb63fd3b73424d2a8af2645122bb6ba5cd
wickedev <orange881217@gmail.com>
2020-03-19 18:17:12 +0900

    remove options examples/with-less config

--- examples/with-less/razzle.config.js
+++ examples/with-less/razzle.config.js
@@ -1,24 +1,5 @@
 'use strict';
 
-const lessOption = {
-  javascriptEnabled: true,
-  modifyVars: { '@info-color': '#1DA57A' },
-};
-
 module.exports = {
-  plugins: [
-    {
-      name: 'less',
-      options: {
-        less: {
-          dev: Object.assign(lessOption, {
-            sourceMap: true,
-          }),
-          prod: Object.assign(lessOption, {
-            sourceMap: false,
-          }),
-        },
-      },
-    },
-  ],
+  plugins: ['less'],
 };
