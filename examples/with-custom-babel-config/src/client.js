48101ce4d52b7baca385c26dffdafafd6eebb16c
Max Moeschinger <m.moeschinger@gmail.com>
2020-04-03 14:28:56 +0200

    fix react-router-dom import warning

--- examples/with-custom-babel-config/src/client.js
+++ examples/with-custom-babel-config/src/client.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { hydrate } from 'react-dom';
-import BrowserRouter from 'react-router-dom/BrowserRouter';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 hydrate(
