69dd8dfa8804d52ee15b8b7999d46e073c8a1ff1
Nima Arefi <nimaarefi@outlook.com>
2020-04-28 05:00:40 +0430

    feat(examples): add with-sass-modules example

---
+++ examples/with-scss-modules/src/App.js
@@ -0,0 +1,12 @@
+import { info } from "./App.module.scss";
+
+import React from "react";
+
+const App = () => (
+  <div>
+    <div>Welcome to Razzle.</div>
+    <div className={info}>Hot reload enabled!!</div>
+  </div>
+);
+
+export default App;
