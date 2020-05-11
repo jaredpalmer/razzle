6cd965482dd28c4584e5f7e0c9ac7637779e6ae5
Ganesh Rathinavel <ganeshrvel@outlook.com>
2020-05-12 03:58:55 +0530

    fixed the chunks 404 error
    The script `assets[key].js` can also be an array which was not handled properly

--- examples/with-vendor-bundle/src/server.js
+++ examples/with-vendor-bundle/src/server.js
@@ -1,32 +1,48 @@
-import App from "./App";
-import React from "react";
-import express from "express";
-import { renderToString } from "react-dom/server";
+import React from 'react';
+import express from 'express';
+import { renderToString } from 'react-dom/server';
+import { StaticRouter } from 'react-router-dom';
+import App from './features/App';
 
-const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
+const assets = require(process.env.RAZZLE_ASSETS_MANIFEST || '');
 
-const scripts = Object.keys(assets).reduce((scripts, key) => {
-  return (
-    scripts + `<script src="${assets[key].js}" defer crossorigin></script>`
-  );
-}, "");
+const scripts = Object.keys(assets).map((key) => {
+  const script = assets[key].js;
 
-console.log(scripts);
+  if (Array.isArray(script)) {
+    return script
+      .map((item) => {
+        return `<script src="${item}" defer crossorigin></script>`;
+      })
+      .join('');
+  }
+
+  return `<script src="${script}" defer crossorigin></script>`;
+});
 
 const server = express();
 
 server
-  .disable("x-powered-by")
-  .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
-  .get("/*", (req, res) => {
-    const markup = renderToString(<App />);
-    res.send(
-      `<!doctype html>
+  .disable('x-powered-by')
+  .use(express.static(process.env.RAZZLE_PUBLIC_DIR || ''))
+  .get('/*', (req, res) => {
+    const context: { url?: string } = {};
+    const markup = renderToString(
+      <StaticRouter context={context} location={req.url}>
+        <App />
+      </StaticRouter>
+    );
+
+    if (context.url) {
+      res.redirect(context.url);
+    } else {
+      res.send(
+        `<!doctype html>
     <html lang="">
     <head>
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta charSet='utf-8' />
-        <title>Welcome to Razzle</title>
+        <title>Welcome to my website</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         ${scripts}
     </head>
@@ -34,7 +50,8 @@ server
         <div id="root">${markup}</div>
     </body>
 </html>`
-    );
+      );
+    }
   });
 
 export default server;
