80eff5af1b0ee3d403906e997db0f0de3b22a935
Aziz Yuldoshev <aziz.yuldoshev@booking.com>
2020-05-12 09:58:56 +0200

    fix: complement CSS loader setup with pre-3.1 configuration
    - Noticed that v3.0 used 'locals' package for CSS modules on
    the server, which maps into `onlyLocals` option in new `css-loader`
    - Fix missing `modules: { auto: true }` for production client-side
    build

--- packages/razzle/config/createConfig.js
+++ packages/razzle/config/createConfig.js
@@ -191,6 +191,7 @@ module.exports = (
                   options: {
                     importLoaders: 1,
                     modules: { auto: true },
+                    onlyLocals: true,
                   },
                 },
               ]
@@ -214,7 +215,8 @@ module.exports = (
                 {
                   loader: require.resolve('css-loader'),
                   options: {
-                    importLoaders: 1
+                    importLoaders: 1,
+                    modules: { auto: true },
                   },
                 },
                 {
