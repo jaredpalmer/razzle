b29f1cda1a3aae6a8944d407ae867a7d25136b2d
Ganesh Rathinavel <ganeshrvel@outlook.com>
2020-05-13 00:32:13 +0530

    Cleaned up the changes

--- examples/with-vendor-bundle/src/server.js
+++ examples/with-vendor-bundle/src/server.js
@@ -1,57 +1,52 @@
-import React from 'react';
-import express from 'express';
-import { renderToString } from 'react-dom/server';
-import { StaticRouter } from 'react-router-dom';
-import App from './features/App';
+import App from "./App";
+import React from "react";
+import express from "express";
+import { renderToString } from "react-dom/server";
 
-const assets = require(process.env.RAZZLE_ASSETS_MANIFEST || '');
+const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
 
-const scripts = Object.keys(assets).map((key) => {
-  const script = assets[key].js;
+const scripts = Object.keys(assets)
+  .map((key) => {
+    const script = assets[key].js;
 
-  if (Array.isArray(script)) {
-    return script
-      .map((item) => {
-        return `<script src="${item}" defer crossorigin></script>`;
-      })
-      .join('');
-  }
+    if (typeof script === 'undefined' || script === null) {
+      return null;
+    }
 
-  return `<script src="${script}" defer crossorigin></script>`;
-});
+    if (Array.isArray(script)) {
+      return script
+        .map((item) => {
+          return `<script src="${item}"></script>`;
+        })
+        .join('');
+    }
+
+    return `<script src="${script}"></script>`;
+  })
+  .filter((a) => typeof a !== 'undefined' || a !== null);
 
 const server = express();
 
 server
-  .disable('x-powered-by')
-  .use(express.static(process.env.RAZZLE_PUBLIC_DIR || ''))
-  .get('/*', (req, res) => {
-    const context: { url?: string } = {};
-    const markup = renderToString(
-      <StaticRouter context={context} location={req.url}>
-        <App />
-      </StaticRouter>
-    );
-
-    if (context.url) {
-      res.redirect(context.url);
-    } else {
-      res.send(
-        `<!doctype html>
+  .disable("x-powered-by")
+  .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
+  .get("/*", (req, res) => {
+    const markup = renderToString(<App />);
+    res.send(
+      `<!doctype html>
     <html lang="">
     <head>
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta charSet='utf-8' />
-        <title>Welcome to my website</title>
+        <title>Welcome to Razzle</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        ${scripts}
+        ${scripts.join('\n')}
     </head>
     <body>
         <div id="root">${markup}</div>
     </body>
 </html>`
-      );
-    }
+    );
   });
 
 export default server;
