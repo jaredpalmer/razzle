70d9cffebb73f875dddef7aba64ffb7a57c76de3
moumniheithem <moumniheithem@gmail.com>
2020-08-13 01:05:26 +0100

    feat: add loader svelte + update file .all-contributorsrc

--- packages/razzle-plugin-svelte/index.js
+++ packages/razzle-plugin-svelte/index.js
@@ -1,6 +1,18 @@
 "use strict";
 
+const WebpackConfigHelpers = require('razzle-dev-utils/WebpackConfigHelpers');
+const Helpers = new WebpackConfigHelpers(process.cwd());
+
 module.exports = function modify(config) {
-  console.log("Razzle comes with Svelte! This package is a stub.");
-  return config;
-};
+  // Add .svelte to extensions
+  config.resolve.extensions = [...config.resolve.extensions, '.svelte'];
+
+  config.module.rules.push({
+    test: /\.svelte$/,
+    loader: require.resolve('svelte-loader'),
+    options: {
+      preprocess:  require('svelte-preprocess')({})
+    }
+  });
+
+});
