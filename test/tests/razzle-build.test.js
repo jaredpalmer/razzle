787474ef8498bec7ddcbd244f57bc728a084d269
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2020-09-01 23:08:29 +0200

    dev-infra: be verbose to see why test fails

--- test/tests/razzle-build.test.js
+++ test/tests/razzle-build.test.js
@@ -8,7 +8,7 @@ const fs = require('fs-extra');
 const shell = require('shelljs');
 const util = require('../fixtures/util');
 
-const silent = true;
+const silent = false;
 shell.config.verbose = !silent;
 shell.config.silent = silent;
 
