b8c4977f2d0de81755fa87454844693de191ab5c
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2020-09-21 15:23:45 +0200

    dev-infra: add better publishing

---
+++ scripts/publish-all.js
@@ -0,0 +1,41 @@
+const yargs = require('yargs');
+const execa = require('execa');
+const fs = require('fs-extra');
+const path = require('path');
+
+let argv = yargs
+.usage('$0 [-r|--release-tag] [-s|--semver-keyword]')
+.command({
+  command: '*',
+  builder: (yargs) => {
+      return yargs.option('r', {
+        alias: 'release-tag',
+        describe: 'the npm release tag',
+        default: 'latest'
+      }).option('s', {
+        alias: 'semver-keyword',
+        describe: 'the semver keyword',
+        default: 'patch'
+      })
+    },
+    handler: argv => {
+      console.log(argv)
+
+      const releaseTag = argv.releaseTag;
+      const semverKeyword = releaseTag !== 'latest' ?
+        argv.semverKeyword == 'patch' ? 'prerelease' : argv.semverKeyword
+        : argv.semverKeyword;
+
+      const lernaCmd = releaseTag == 'latest' ?
+        `lerna version ${semverKeyword} --force-publish --no-push` :
+        `lerna version ${semverKeyword} --preid ${releaseTag} --force-publish --no-push`;
+      execa(lernaCmd, {shell: true, stdio: 'inherit' }).then(async ()=>{
+        const latestTagId = (await execa('git rev-list --tags --max-count=1', {shell: true})).stdout;
+        const latestTag = (await execa(`git describe --tags ${latestTagId}`, {shell: true})).stdout;
+        await execa(`git tag -d ${latestTag}`, {shell: true, stdio: 'inherit' });
+        await execa(`git reset --soft HEAD~1`, {shell: true, stdio: 'inherit' });
+      });
+    }
+  })
+  .help()
+  .argv
