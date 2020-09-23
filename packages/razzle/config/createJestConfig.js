4f840f2e04f53a86c073f00ef144111e4f17fad1
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2020-09-23 20:01:41 +0200

    fix(razzle): use let so config can be redefined

--- packages/razzle/config/createJestConfig.js
+++ packages/razzle/config/createJestConfig.js
@@ -33,7 +33,7 @@ module.exports = (
 
     // TODO: I don't know if it's safe or not to just use / as path separator
     // in Jest configs. We need help from somebody with Windows to determine this.
-    const config = Object.assign(
+    let config = Object.assign(
       {
         collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.d.ts'],
 
