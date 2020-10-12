f89558fa78e47b71131422f0f01c8d239ff21944
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2020-10-12 19:30:37 +0200

    fix(examples): fix all examples used in tests

--- examples/with-jsconfig-paths/src/server.js
+++ examples/with-jsconfig-paths/src/server.js
@@ -5,6 +5,20 @@ import { renderToString } from 'react-dom/server';
 
 const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
 
+const cssLinksFromAssets = (assets, entrypoint) => {
+  return assets.entrypoints[entrypoint] ? assets.entrypoints[entrypoint].css ?
+  assets.entrypoints[entrypoint].css.map(asset=>
+    `<link rel="stylesheet" href="${asset}">`
+  ).join('') : '' : '';
+};
+
+const jsScriptTagsFromAssets = (assets, entrypoint, extra = '') => {
+  return assets.entrypoints[entrypoint] ? assets.entrypoints[entrypoint].js ?
+  assets.entrypoints[entrypoint].js.map(asset=>
+    `<script src="${asset}"${extra}></script>`
+  ).join('') : '' : '';
+};
+
 const server = express();
 
 server
@@ -21,18 +35,15 @@ server
         <meta charSet='utf-8' />
         <title>Welcome to Razzle</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        ${
-          assets.client.css
-            ? `<link rel="stylesheet" href="${assets.client.css}">`
-            : ''
-        } 
+        ${cssLinksFromAssets(assets, 'client')}
     </head>
     <body>
-        <div id="root">${markup}</div>    
-        <script src="${assets.client.js}" defer crossorigin></script>
+        <div id="root">${markup}</div>
+        ${jsScriptTagsFromAssets(assets, 'client', ' defer crossorigin')}
     </body>
 </html>`
     );
   });
+
 
 export default server;
