e9a8a00c89910d73398cadd9f9cc4ba4614a44e6
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2020-10-25 16:29:53 +0100

    examples: remove with-react-loadable since it is not maintained

--- examples/with-react-loadable/src/Logo.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import logo from './react.svg';
-
-const Logo = () => <img src={logo} className="Home-logo" alt="logo" />;
-
-export default Logo;
--- examples/with-react-loadable/src/Welcome.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Welcome = () => <h2>Welcome to Razzle</h2>;
-
-export default Welcome;
