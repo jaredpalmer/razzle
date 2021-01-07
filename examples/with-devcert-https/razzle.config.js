bebe5e6e02c651c614c9ff19b7ef074009b7a5c8
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2021-01-07 16:10:41 +0100

    fix(examples): use more hooks and correct options

--- examples/with-devcert-https/razzle.config.js
+++ examples/with-devcert-https/razzle.config.js
@@ -1,26 +1,28 @@
 'use strict';
 
 module.exports = {
-  modifyWebpackOptions(opts) {
-    const options = opts.webpackOptions;
+  modifyOptions(opts) { // use modifyOptions so certificateFor is called once
+    const options = opts.options.razzleOptions;
     return new Promise(async (resolve) => {
       const httpsCredentials = await devcert.certificateFor('localhost');
       const stringHttpsCredentials = {
         key: httpsCredentials.key.toString(),
         cert: httpsCredentials.cert.toString()
       };
-      if (opts.env.target === 'node' && opts.env.dev) {
-        options.definePluginOptions.HTTPS_CREDENTIALS = JSON.stringify(stringHttpsCredentials);
-      }
-      if (opts.env.target === 'web' && opts.env.dev) {
-        options.HTTPS_CREDENTIALS = httpsCredentials;
-      }
+      options.HTTPS_CREDENTIALS = stringHttpsCredentials;
       resolve(options);
     });
   },
+  modifyWebpackOptions(opts) {
+    const options = opts.options.razzleOptions;
+    if (opts.env.target === 'node' && opts.env.dev) {
+      options.definePluginOptions.HTTPS_CREDENTIALS = JSON.stringify(options.HTTPS_CREDENTIALS);
+    }
+    return options;
+  },
   modifyWebpackConfig(opts) {
-    const config = opts.webpackConfig;
-    const options = opts.webpackOptions;
+    const config = opts.options.webpackConfig;
+    const options = opts.options.razzleOptions;
     if (opts.env.target === 'web' && opts.env.dev) {
       config.devServer.https = options.HTTPS_CREDENTIALS;
     }
