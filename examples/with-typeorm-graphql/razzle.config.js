20d80fa1f4dce33a713e360412ce05c44b153416
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2021-02-01 19:21:33 +0100

    example: work on some ideas on how to fix this

--- examples/with-typeorm-graphql/razzle.config.js
+++ examples/with-typeorm-graphql/razzle.config.js
@@ -2,6 +2,10 @@
 
 
 module.exports = {
+  options: {
+    verbose: true,
+    enableTargetBabelrc: true
+  },
   modifyWebpackConfig({
     env: {
       target, // the target 'node' or 'web'
@@ -10,16 +14,36 @@ module.exports = {
     webpackConfig, // the created webpack config
     webpackObject, // the imported webpack node module
     options: {
-      pluginOptions, // the options passed to the plugin ({ name:'pluginname', options: { key: 'value'}})
       razzleOptions, // the modified options passed to Razzle in the `options` key in `razzle.config.js` (options: { key: 'value'})
       webpackOptions, // the modified options that was used to configure webpack/ webpack loaders and plugins
     },
     paths, // the modified paths that will be used by Razzle.
   }) {
     if (target === 'node') {
-
+      // webpackConfig.output.library = {
+      //   type: 'umd'
+      // };
+      // webpackConfig.output.libraryTarget = 'var';
+      // webpackConfig.optimization.minimize = false;
     }
     return webpackConfig;
   },
-  plugins: ['typescript']
+  modifyWebpackOptions({
+    env: {
+      target, // the target 'node' or 'web'
+      dev, // is this a development build? true or false
+    },
+    webpackObject, // the imported webpack node module
+    options: {
+      razzleOptions, // the modified options passed to Razzle in the `options` key in `razzle.config.js` (options: { key: 'value'})
+      webpackOptions, // the modified options that was used to configure webpack/ webpack loaders and plugins
+    },
+    paths, // the modified paths that will be used by Razzle.
+  }) {
+    // webpackOptions.jsOutputFilename = `[name].mjs`;
+    // webpackOptions.jsOutputChunkFilename = `[name].chunk.mjs`;
+    //webpackOptions.terserPluginOptions.terserOptions.compress.ecma = 6;
+    return webpackOptions;
+  },
+  plugins: [{name:'typescript',options: {useBabel:true}}]
 };
