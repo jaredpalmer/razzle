9a9ee5e967c75d5db20e5a46c1aeb3c2e98516f9
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2021-02-04 20:32:13 +0100

    fix(with-typeorm-graphql): do a dual build to make it work

--- examples/with-typeorm-graphql/razzle.config.js
+++ examples/with-typeorm-graphql/razzle.config.js
@@ -19,32 +19,32 @@ module.exports = {
     paths, // the modified paths that will be used by Razzle.
   }) {
     if (target === 'node') {
-
-
-      webpackConfig.output.library = 'RAZZLEAPP';
-      webpackConfig.output.libraryTarget = 'var';
-      webpackConfig.target = 'node14';
-      webpackConfig.resolve.mainFields = ['main', 'module'];
-      webpackConfig.resolve.extensions= ['.ts', '.tsx', '.js'];
-      // webpackConfig.plugins.push({
-      //   apply: (compiler) => {
-      //     const esmExports = `export default RAZZLEAPP;`;
-      //     const esmExportsn = ``;
-      //     const fakeRequire = `const require = (what) => { return import(what); }`;
-      //     compiler.hooks.thisCompilation.tap('AddESMExports', compilation => {
-      //       compilation.hooks.processAssets.tap({ name: 'AddESMExports', stage: compiler.webpack.Compilation.PROCESS_ASSETS_STAGE_ADDITIONS }, chunks => {
-      //         Object.keys(chunks).forEach(fileName => {
-      //           compilation.updateAsset(fileName, content => new compiler.webpack.sources.ConcatSource(fakeRequire, '\n', content, '\n', esmExportsn));
-      //         });
-      //       });
-      //     });
-      //   }
-      // });
-      if (!dev) {
-        webpackConfig.optimization.minimize = false;
-      }
-      // webpackConfig.output.environment = {
-      //   module: true,
+      //
+      //
+      // webpackConfig.output.library = 'RAZZLEAPP';
+      // webpackConfig.output.libraryTarget = 'var';
+      // webpackConfig.target = 'node14';
+      // webpackConfig.resolve.mainFields = ['main', 'module'];
+      // // webpackConfig.resolve.extensions= ['.ts', '.tsx', '.js'];
+      // // webpackConfig.plugins.push({
+      // //   apply: (compiler) => {
+      // //     const esmExports = `export default RAZZLEAPP;`;
+      // //     const esmExportsn = ``;
+      // //     const fakeRequire = `const require = (what) => { return import(what); }`;
+      // //     compiler.hooks.thisCompilation.tap('AddESMExports', compilation => {
+      // //       compilation.hooks.processAssets.tap({ name: 'AddESMExports', stage: compiler.webpack.Compilation.PROCESS_ASSETS_STAGE_ADDITIONS }, chunks => {
+      // //         Object.keys(chunks).forEach(fileName => {
+      // //           compilation.updateAsset(fileName, content => new compiler.webpack.sources.ConcatSource(fakeRequire, '\n', content, '\n', esmExportsn));
+      // //         });
+      // //       });
+      // //     });
+      // //   }
+      // // });
+      // if (!dev) {
+      //   webpackConfig.optimization.minimize = false;
+      // }
+      // // webpackConfig.output.environment = {
+      // //   module: true,
       //   dynamicImport: true,
       // };
       // webpackConfig.experiments = {
@@ -66,9 +66,9 @@ module.exports = {
     },
     paths, // the modified paths that will be used by Razzle.
   }) {
-    webpackOptions.jsOutputFilename = `[name].js`;
-    webpackOptions.jsOutputChunkFilename = `[name].chunk.js`;
-    // //webpackOptions.terserPluginOptions.terserOptions.compress.ecma = 6;
+    // webpackOptions.jsOutputFilename = `[name].js`;
+    // webpackOptions.jsOutputChunkFilename = `[name].chunk.js`;
+    // // //webpackOptions.terserPluginOptions.terserOptions.compress.ecma = 6;
     return webpackOptions;
   },
   //plugins: [{name:'typescript'}]
