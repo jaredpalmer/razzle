76d91d259b56b7c965eb141ba2cc52eaf59d1ed3
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2021-02-07 21:24:11 +0100

    Update razzle-build.test.js

--- test/e2e/razzle-build.test.js
+++ test/e2e/razzle-build.test.js
@@ -8,7 +8,7 @@ const fs = require('fs-extra');
 const shell = require('shelljs');
 const util = require('../fixtures/util');
 
-const silent = false;
+const silent = !process.env.NOISY_TESTS;
 shell.config.verbose = !silent;
 shell.config.silent = silent;
 
