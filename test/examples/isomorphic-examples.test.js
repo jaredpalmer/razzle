580b90782de659482bb3d103a3b8471c9de409e6
fivethreeo <oyvind.saltvik@gmail.com>
2021-02-09 23:23:27 +0100

    dev-infra: working puppeteer setup

--- test/examples/isomorphic-examples.test.js
+++ test/examples/isomorphic-examples.test.js
@@ -3,7 +3,10 @@
 */
 'use strict';
 
-var assert = require('assert');
+const puppeteer = require('puppeteer');
+const terminate = require('terminate');
+
+const assert = require('assert');
 const os = require('os');
 const fs = require('fs-extra');
 const execa = require('execa');
@@ -22,6 +25,20 @@ const silent = !process.env.NOISY_TESTS;
 
 const examples = ['basic'];
 
+let browser;
+let page;
+
+beforeAll(async function(done) {
+  browser = await puppeteer.launch();
+  page = await browser.newPage();
+  done();
+});
+
+afterAll(async function(done) {
+  await browser.close();
+  done();
+});
+
 describe(`tests for isomorphic examples`, () => {
 
   examples.forEach(example => {
@@ -29,8 +46,9 @@ describe(`tests for isomorphic examples`, () => {
 
       let tempDir;
 
-      beforeAll(function(done) {
-        mkdtemp(mkdtempTpl, (err, directory) => {
+      beforeAll(async function(done) {
+
+          mkdtemp(mkdtempTpl, (err, directory) => {
           tempDir = directory;
           copy(path.join(rootDir, 'examples', example), tempDir, function(error, results) {
             if (error) {
@@ -60,24 +78,44 @@ describe(`tests for isomorphic examples`, () => {
         })
       }, 300000);
 
-      it(`should build successfully`, function(done) {
+      it(`should build successfully`, async function(done) {
         execa("yarn", ["build"], {stdio: 'inherit', cwd: tempDir })
         .then(({exitCode})=>{
           assert.equal(exitCode, 0)
-          done();
-        })
-      });
 
-      jest.setTimeout(300000);
-
-      it(`should start devserver and exit`, function(done) {
-        const subprocess = execa("yarn", ["start"], {stdio: 'inherit', cwd: tempDir })
-        subprocess.then(({exitCode})=>{
-          assert.equal(exitCode, 0)
           done();
         })
       }, 300000);
 
+      jest.setTimeout(300000);
+
+      it(`should start devserver and exit`, async function(done) {
+        const subprocess = execa("yarn", ["start"], {stdio: 'inherit', cwd: tempDir })
+        await new Promise((r) => setTimeout(r, 5000));
+        await page.goto('http://localhost:3000/');
+        await page.screenshot({ path: 'example.png' });
+        await page.goto('https://google.com');
+
+        terminate(subprocess.pid, 'SIGINT', { timeout: 1000 }, () => {
+          terminate(subprocess.pid);
+          done();
+        });
+
+        // subprocess.then(({killed})=>{
+        //   assert.equal(killed, true)
+        // })
+      }, 300000);
+
+      jest.setTimeout(300000);
+      //
+      // it(`should start devserver and the page should load js`, async function(done) {
+
+      //   const subprocess = execa("yarn", ["start"], {stdio: 'inherit', cwd: tempDir })
+      //   subprocess.then(({exitCode})=>{
+      //     assert.equal(exitCode, 0)
+          // done();
+      //   })
+      // }, 300000);
     });
   });
 });
