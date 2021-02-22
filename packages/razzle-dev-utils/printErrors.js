6f7b4dd642dccf5f83572b070f6eed5d2aa9637f
Simeon Cheeseman <simeon@tablecheck.com>
2021-02-22 11:12:25 +0900

    fix(razzle-dev-utils): correct webpack 4 format
    Webpack 4 has a different output format for warnings and errors than webpack 5. Weirdly enough this is a string and array of strings.
    
    Closes #1558

--- packages/razzle-dev-utils/printErrors.js
+++ packages/razzle-dev-utils/printErrors.js
@@ -12,12 +12,17 @@ function printErrors(summary, errors, verbose) {
   console.log(chalk.red(summary));
   console.log();
   errors.forEach(err => {
-    if (err.message) {
-      console.error(err.message);
-    }
-    console.error(err.stack || err);
-    if (err.details) {
-      console.error(err.details);
+    if (Array.isArray(err)) {
+      // webpack 4 format
+      console.error(err.join(''));
+    } else {
+      if (err.message) {
+        console.error(err.message);
+      }
+      console.error(err.stack || err);
+      if (err.details) {
+        console.error(err.details);
+      }
     }
     console.log();
   });
