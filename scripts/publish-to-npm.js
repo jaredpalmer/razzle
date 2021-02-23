3a742856d3082641ca46b73e7a27fb0d60b77245
fivethreeo <oyvind.saltvik@gmail.com>
2021-02-23 16:37:56 +0100

    dev-infra: use correct registry

--- scripts/publish-to-npm.js
+++ scripts/publish-to-npm.js
@@ -34,7 +34,7 @@ let argv = yargs
 			).flat();
 
 			await Promise.all(packageDirs.map((item) => {
-				const publishCmd = `npm publish --tag ${argv.tag}`;
+				const publishCmd = `npm publish --tag ${argv.tag} --registry=https://registry.npmjs.org/ `;
 				return execa(publishCmd, { shell: true, stdio: 'inherit', cwd: path.join(rootDir, item) });
 			}))
 
