a239ac105b5e7e00fe7f8238e764a1d2a5e64ddd
fivethreeo <oyvind.saltvik@gmail.com>
2021-03-05 16:34:16 +0100

    examples: update more examples with assets loading

--- examples/with-elm/src/server.js
+++ examples/with-elm/src/server.js
@@ -7,40 +7,58 @@ const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
 const model = { counter: 5 };
 const options = { model : model, decoder: "App.decodeModel" };
 
-const server = express();
-server
-.disable('x-powered-by')
-.use(express.static(process.env.RAZZLE_PUBLIC_DIR))
-.get('/*', (req, res) => {
-  elmStaticHtml(process.cwd(), "App.view", options)
-  .then((generatedHtml) => {
-    const markup = generatedHtml;
-    res.send(
-      `<!doctype html>
-      <html lang="">
-        <head>
-            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-            <meta charSet='utf-8' />
-            <title>Welcome to Razzle</title>
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            ${assets.client.css
-              ? `<link rel="stylesheet" href="${assets.client.css}">`
-              : ''}          
-            
-        </head>
-        <body>
-            <div id="root">${markup}</div>
-        </body>
-        ${process.env.NODE_ENV === 'production'
-        ? `<script src="${assets.client.js}"></script>`
-        : `<script src="${assets.client.js}"></script>`}
-      </html>`
-    );
-  }).catch((error) => {
-    console.log(error);
-    res.status(500).send(`<h1>An error ocurred on server, please try later, or contact support</h1>`);
-  });
+const cssLinksFromAssets = (assets, entrypoint) => {
+  return assets[entrypoint] ? assets[entrypoint].css ?
+  assets[entrypoint].css.map(asset=>
+    `<link rel="stylesheet" href="${asset}">`
+  ).join('') : '' : '';
+};
 
-});
+const jsScriptTagsFromAssets = (assets, entrypoint, extra = '') => {
+  return assets[entrypoint] ? assets[entrypoint].js ?
+  assets[entrypoint].js.map(asset=>
+    `<script src="${asset}"${extra}></script>`
+  ).join('') : '' : '';
+};
+
+export const renderApp = (req, res) => {
+  return new Promise((resolve, reject) {
+    elmStaticHtml(process.cwd(), "App.view", options)
+    .then((markup) => {
+      const html =
+        // prettier-ignore
+        `<!doctype html>
+      <html lang="">
+      <head>
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta charSet='utf-8' />
+          <title>Welcome to Razzle</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          ${cssLinksFromAssets(assets, 'client')}
+      </head>
+      <body>
+          <div id="root">${markup}</div>
+          ${jsScriptTagsFromAssets(assets, 'client', ' defer crossorigin')}
+      </body>
+    </html>`;
+
+
+      resolve({ html, status: 200 });
+    }).catch((error) => {
+      resolve({ html: `<h1>An error ocurred on server, please try later, or contact support</h1>`, status: 200 });
+    });
+  })
+};
+
+
+const server = express();
+
+server
+  .disable('x-powered-by')
+  .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
+  .get('/*', async (req, res) => {
+    const { html, status } = await renderApp(req, res);
+    res.status(status).send(html);
+  });
 
 export default server;
--- examples/with-hyperapp/src/server.js
+++ examples/with-hyperapp/src/server.js
@@ -1,20 +1,36 @@
 /** @jsx h */
 import express from 'express';
 import { app } from 'hyperapp';
+
 import { withRender } from '@hyperapp/render';
 
 import { state, actions, view } from './main';
 
 const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
 
+const cssLinksFromAssets = (assets, entrypoint) => {
+  return assets[entrypoint] ? assets[entrypoint].css ?
+  assets[entrypoint].css.map(asset=>
+    `<link rel="stylesheet" href="${asset}">`
+  ).join('') : '' : '';
+};
+
+const jsScriptTagsFromAssets = (assets, entrypoint, extra = '') => {
+  return assets[entrypoint] ? assets[entrypoint].js ?
+  assets[entrypoint].js.map(asset=>
+    `<script src="${asset}"${extra}></script>`
+  ).join('') : '' : '';
+};
+
 const server = express();
+
 server
   .disable('x-powered-by')
   .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
   .get('/*', (req, res) => {
     const markup = withRender(app)(state, actions, view).toString();
-
-    res.status(200).send(
+    res.send(
+      // prettier-ignore
       `<!doctype html>
     <html lang="">
     <head>
@@ -22,19 +38,13 @@ server
         <meta charSet='utf-8' />
         <title>Welcome to Razzle</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        ${
-          assets.client.css
-            ? `<link rel="stylesheet" href="${assets.client.css}">`
-            : ''
-        }
-        ${
-          process.env.NODE_ENV === 'production'
-            ? `<script src="${assets.client.js}" defer></script>`
-            : `<script src="${assets.client.js}" defer crossorigin></script>`
-        }
+        ${cssLinksFromAssets(assets, 'client')}
     </head>
     <body>
-        ${markup}
+        <div id="root">${markup}</div>
+        ${process.env.NODE_ENV === 'production'
+            ? jsScriptTagsFromAssets(assets, 'client', ' defer')
+            : jsScriptTagsFromAssets(assets, 'client', ' defer crossorigin')}
     </body>
 </html>`
     );
