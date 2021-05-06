jaredpalmer/razzle

commit f1fdf1c207f3d252e6fd2e80b6afab3e69044aa3

feat(razzle): Make package.json optional
In some setups there may only be one package.json in the root directory,
and multiple apps (using RAZZLE_APP_PATH) are developed in separate
subdirectories.

Before this change, razzle expected a package.json to be present in each
directory - even if it contained an empty object.

packages/razzle/config/paths.js
@@ -3,6 +3,8 @@
 const path = require('path');
 const fs = require('fs');
 const url = require('url');
+const clearConsole = require('react-dev-utils/clearConsole');
+const logger = require('razzle-dev-utils/logger');
 
 // Make sure any symlinks in the project folder are resolved:
 // https://github.com/facebookincubator/create-react-app/issues/637
@@ -22,8 +24,24 @@ function ensureSlash(path, needsSlash) {
   }
 }
 
-const getPublicUrl = appPackageJson =>
-  envPublicUrl || require(appPackageJson).homepage;
+const getPublicUrl = appPackageJson => {
+  if (envPublicUrl) {
+    return envPublicUrl;
+  }
+  
+  if (fs.existsSync(appPackageJson)) {
+    try {
+      packageJson = require(appPackageJson)
+      return packageJson.homepage;
+    } catch (e) {
+      clearConsole();
+      logger.error('Invalid package.json.', e);
+      process.exit(1);
+    }
+  }
+
+  return undefined;
+};
 
 function getServedPath(appPackageJson) {
   const publicUrl = getPublicUrl(appPackageJson);

packages/razzle/config/createJestConfig.js
@@ -3,6 +3,8 @@
 const fs = require('fs');
 const chalk = require('chalk');
 const jest = require('jest');
+const clearConsole = require('react-dev-utils/clearConsole');
+const logger = require('razzle-dev-utils/logger');
 
 // first search for setupTests.ts file
 // if .ts file not exists then looks for setupTests.js
@@ -66,7 +68,18 @@ module.exports = (
     if (rootDir) {
       config.rootDir = rootDir;
     }
-    const overrides = Object.assign({}, require(paths.appPackageJson).jest);
+
+    let overrides = {};
+    if (fs.existsSync(paths.appPackageJson)) {
+      try {
+        overrides = Object.assign({}, require(paths.appPackageJson).jest);
+      } catch (e) {
+        clearConsole();
+        logger.error('Invalid package.json.', e);
+        process.exit(1);
+      }
+    }
+
     const supportedKeys = [
       'collectCoverageFrom',
       'coverageReporters',

commit 26cd24c179820fbc4b2ff67c657babd84c72a349

fix(razzle): Undefined error in getPublicUrl
Introduced in f1fdf1c207f3d252e6fd2e80b6afab3e69044aa3

packages/razzle/config/paths.js
@@ -31,7 +31,7 @@ const getPublicUrl = appPackageJson => {
   
   if (fs.existsSync(appPackageJson)) {
     try {
-      packageJson = require(appPackageJson)
+      const packageJson = require(appPackageJson)
       return packageJson.homepage;
     } catch (e) {
       clearConsole();