ac03265c6226f2aacb1a5bdb1704a2bd9e76ec62
Reiner Gerecke <reiner.gerecke@momox.biz>
2021-05-07 17:00:38 +0200

    feat(razzle): Align unhandled rejection output across all entrypoints

--- packages/razzle/scripts/build.js
+++ packages/razzle/scripts/build.js
@@ -1,13 +1,6 @@
 #! /usr/bin/env node
 'use strict';
 
-// Makes the script crash on unhandled rejections instead of silently
-// ignoring them. In the future, promise rejections that are not handled will
-// terminate the Node.js process with a non-zero exit code.
-process.on('unhandledRejection', err => {
-  throw err;
-});
-
 const mri = require('mri');
 
 const argv = process.argv.slice(2);
@@ -35,6 +28,14 @@ const measureFileSizesBeforeBuild =
   FileSizeReporter.measureFileSizesBeforeBuild;
 const printFileSizesAfterBuild = FileSizeReporter.printFileSizesAfterBuild;
 
+// Makes the script crash on unhandled rejections instead of silently
+// ignoring them. In the future, promise rejections that are not handled will
+// terminate the Node.js process with a non-zero exit code.
+process.on('unhandledRejection', err => {
+  clearConsole();
+  logger.error('Unexpected error', err);
+  process.exit(1);
+});
 
 loadRazzleConfig(webpack).then(
   async ({ razzle, razzleOptions, webpackObject, plugins, paths }) => {
