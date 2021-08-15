4d39060ea51ca346762df258b324d3660b3b61b3
David <54036124+dvprrsh@users.noreply.github.com>
2021-08-15 17:13:19 +0100

    examples(with-fastify): comment alteration

--- examples/with-fastify/src/server.tsx
+++ examples/with-fastify/src/server.tsx
@@ -57,7 +57,7 @@ const app = fastify()
  * This is a similar implementation to the one found in the Fastify docs,
  * which can be found [here]{@link https://www.fastify.io/docs/latest/Serverless/#should-you-use-fastify-in-a-serverless-platform#Vercel}.
  * The `app#ready()` is to indicate that all plugins have been loaded and the server is ready,
- * then the `app#server#emit` method is called to handle the incoming request and Fastify handles it from there.
+ * then the `app#server#emit()` method is called to handle the incoming request and Fastify handles it from there.
  */
 export default async (req: IncomingMessage, res: ServerResponse): Promise<void> => {
   await app.ready();
