8bb9b036a3aae1b3069203213165752197f29600
Alexander Domrachev <alexanderdomrachev@gmail.com>
2021-08-25 15:28:26 +0300

    Fix .eslintrc.js

--- examples/with-typescript-plugin/.eslintrc.js
+++ examples/with-typescript-plugin/.eslintrc.js
@@ -1,56 +1,55 @@
-module.exports =
-{
-  "env": {
-    "node": true,
-    "browser": true,
-    "es6": true
+module.exports = {
+  env: {
+    node: true,
+    browser: true,
+    es6: true,
   },
-  "globals": {
-    "Atomics": "readonly",
-    "SharedArrayBuffer": "readonly"
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
   },
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "ecmaVersion": 2018,
-    "sourceType": "module"
+    ecmaVersion: 2018,
+    sourceType: "module",
   },
-  "plugins": ["react", "@typescript-eslint", "prettier"],
-  "extends": [
+  plugins: ["react", "@typescript-eslint", "prettier"],
+  extends: [
     "plugin:react/recommended",
     "airbnb",
     "plugin:prettier/recommended",
-    "no-use-before-define": "off",
-    "@typescript-eslint/no-use-before-define": ["error"],
     "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended"
+    "plugin:@typescript-eslint/recommended",
   ],
-  "rules": {
+  rules: {
     "import/extensions": [
       "error",
       "ignorePackages",
       {
-        "js": "never",
-        "jsx": "never",
-        "ts": "never",
-        "tsx": "never"
-      }
+        js: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never",
+      },
     ],
     "prettier/prettier": "error",
+    "no-use-before-define": "off",
+    "@typescript-eslint/no-use-before-define": ["error"],
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/no-unused-vars": "off",
     "react/jsx-filename-extension": [
       1,
-      { "extensions": [".js", ".jsx", ".ts", ".tsx"] }
-    ]
+      { extensions: [".js", ".jsx", ".ts", ".tsx"] },
+    ],
   },
-  "settings": {
+  settings: {
     "import/resolver": {
-      "node": {
-        "extensions": [".js", ".jsx", ".ts", ".tsx"]
-      }
-    }
-  }
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
 };
