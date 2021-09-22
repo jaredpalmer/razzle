cfe825c4814e5a9dd3ff8149578afe2f813a33a3
Budi Adiono <budiadiono@gmail.com>
2021-09-22 10:18:38 +0700

    fix: StaticRouterContext should be imported from "react-router"

--- examples/with-typescript/src/server.tsx
+++ examples/with-typescript/src/server.tsx
@@ -1,7 +1,8 @@
 import express from 'express';
 import React from 'react';
 import { renderToString } from 'react-dom/server';
-import { StaticRouter, StaticRouterContext } from 'react-router-dom';
+import { StaticRouterContext } from "react-router";
+import { StaticRouter } from 'react-router-dom';
 
 import App from './App';
 
