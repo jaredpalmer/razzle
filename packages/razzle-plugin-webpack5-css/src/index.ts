89be13fa09ec54ebfc3d2e4d8faa5cbb52cc2d72
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2021-12-16 16:13:33 +0100

    chore: add turbo

---
+++ packages/razzle-plugin-webpack5-css/src/index.ts
@@ -0,0 +1,50 @@
+import { PluginOptions, Plugin } from "./types";
+
+import type * as types from "./types";
+export { types }
+
+const plugin: Plugin = {
+  name: "webpack5-css",
+  defaultOptions: {},
+  modifyConfig: (
+    pluginOptions,
+    razzleConfig,
+    razzleContext,
+    webpackOptions,
+    webpackConfig
+  ) => {
+    webpackConfig?.module?.rules?.push(
+      ...[
+        {
+          test: /\.js$/i,
+          include: [razzleContext.paths.appSrc], //.concat(additionalIncludes)
+          use: [
+            {
+              loader: "razzle-plugin-webpack5-babel/loader",
+              options: {
+                isServer: webpackOptions.isNode,
+                cwd: razzleContext.paths.appPath,
+                cache: true,
+                babelPresetPlugins: [],
+                hasModern: false, // !!config.experimental.modern,
+                development: webpackOptions.isDev,
+                hasReactRefresh: false,
+              },
+            },
+          ],
+        },
+      ]
+    );
+    return webpackConfig;
+  },
+};
+
+export default function (options: PluginOptions): {
+  plugin: Plugin,
+  options: PluginOptions
+} {
+  return {
+    plugin: plugin,
+    options: { ...(plugin.defaultOptions || {}), ...options },
+  };
+}
