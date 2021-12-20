0905ae77ae57b5f00efa8d0c97bc7ebdda841ffe
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2021-12-20 19:05:18 +0100

    fix: make express bundling work

--- examples/basic-serverless/razzle.config.js
+++ examples/basic-serverless/razzle.config.js
@@ -3,5 +3,25 @@
 module.exports = {
   options: {
     buildType: 'iso-serverless',
+    modifyWebpackConfig(opts) {
+      const config = opts.webpackConfig;
+
+      if (opts.env.target === 'node') {
+        config.plugins.push(
+          new opts.webpackObject.ContextReplacementPlugin(
+                                               // we want to replace context
+            /express\/lib/,                    // and replace all searches in
+                                               // express/lib/*
+            resolve('node_modules'),           // to look in folder 'node_modules'
+            {                                  // and return a map
+              'ejs': 'ejs'                     // which resolves request for 'ejs'
+            }                                  // to module 'ejs'
+          )                                    // __webpack_require__(...)(mod)
+          // we set `mod = 'ejs'`
+        )
+      }
+
+      return config;
+    },
   }
 };
